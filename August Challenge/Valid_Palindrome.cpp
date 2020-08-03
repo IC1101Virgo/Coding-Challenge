a4a686897e6f162c192510a3beb3c9f979c54131
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2020-08-03 22:46:12 +0530

    3 August 2020

---
+++ August Challenge/Valid_Palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        char[] c = s.toCharArray();
+        int i = 0;
+        int j = c.length - 1;
+        while (i < j) {
+            while (i < c.length && !Character.isLetterOrDigit(c[i])) {
+                i++;
+            }
+            while (j >= 0 && !Character.isLetterOrDigit(c[j])) {
+                j--;
+            }
+            if (i < c.length && j >= 0 && Character.toLowerCase(c[i]) != Character.toLowerCase(c[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
