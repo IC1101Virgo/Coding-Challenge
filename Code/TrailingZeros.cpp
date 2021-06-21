016f27b67111fed245527fc1698392a9c583b52a
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2021-06-21 08:29:04 +0530

    Create TrailingZeros.cpp

---
+++ Code/TrailingZeros.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int res=0;
+        
+        for(int i=5;i<=n;i=i*5)
+            res+=(n/i);
+        
+        return res;
+    }
+};
