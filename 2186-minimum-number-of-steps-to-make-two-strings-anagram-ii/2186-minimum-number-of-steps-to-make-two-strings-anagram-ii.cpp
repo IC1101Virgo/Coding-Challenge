955504eefd47622f3627e98690913c2b9a082285
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-02-27 10:54:14 +0530

    Time: 131 ms (66.67%), Space: 28.9 MB (33.33%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,33 +1,11 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int a=0,b=0;
-        
-        map<char,int>ms, mt;
-        
-        for(int i=0;i<s.size();i++)
-            ms[s[i]]++;
-        
-        for(int i=0;i<t.size();i++)
-            mt[t[i]]++;
-        
-        for(int i=0;i<t.size();i++){
-            if(ms.find(t[i])==ms.end())
-                a++;
-        }
-        
-        for(int i=0;i<s.size();i++){
-            if(mt.find(s[i])==mt.end())
-                b++;
-        }
-        
-        for(auto x:ms){
-            for(auto y:mt){
-            if(x.first==y.first && x.second!=y.second)
-                a+=abs(x.second-y.second);
-            }
-        }
-        
-        return a+b;
+      int ans = 0;
+    vector<int> sCounter(26, 0), tCounter(26, 0);
+    for(char ch: s) sCounter[ch - 'a']++;
+    for(char ch: t) tCounter[ch - 'a']++;
+    for (int i = 0; i < 26; i++) ans += abs(sCounter[i] - tCounter[i]);
+    return ans;
     }
 };
