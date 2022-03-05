c30f7d53eff856dcc93dc5c69565913d112de521
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-05 09:02:18 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int v, vector<int> adj[]) {
+        // Code here
+        vector<int> res;
+        queue<int> q;
+        
+        vector<bool> vis(v,0);
+        int x=0;
+        q.push(x);
+        vis[0]=1;
+        while(!q.empty()){
+            int t=q.front();
+            res.push_back(t);
+            q.pop();
+            for(auto ed:adj[t]){
+                if(vis[ed]==0)
+                q.push(ed);
+                vis[ed]=1;
+            }
+        }
+        return res;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
