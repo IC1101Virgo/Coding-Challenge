7612f8c2b5902fa42ff07dd96a165a4ee9f9ec3b
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-06 11:45:28 +0530

    Time: 1402 ms (7.69%), Space: 277.7 MB (7.69%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,47 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private: 
-		// Map to store node value and its pointer
-	    unordered_map<int, TreeNode*> nodeMap;
-private: 
-	// Helper function to getNode by value
-	TreeNode* getNode(int val) {
-        TreeNode* node = NULL;
-		// If node is not present in map, 
-		// Then create a new node, and store it in map
-        if(nodeMap.find(val) == nodeMap.end()) {
-            node = new TreeNode(val);
-            nodeMap[val] = node;
-        }
-		// If node is already present in map then return it
-        else {
-            node = nodeMap[val];
-        }
-        return node;
-    }
 public:
-       
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+    TreeNode* createBinaryTree(vector<vector<int>>& a) {
+        unordered_map<int,TreeNode*>nodetree;
+        unordered_map<int,bool>ischild;
         
-        unordered_set<int> isChild; // used to track if a node is child of some other node
-        for(auto& description : descriptions) {
-            int val = description[0], child = description[1];
-            bool left = (description[2]);
+        for(auto &v:a){
+            if(nodetree.count(v[0])==0){
+                TreeNode* par=new TreeNode(v[0]);
+                nodetree[v[0]]=par;
+            }
             
-			// Get node and then connect to left or right child accordingly
-            TreeNode* node = getNode(val);
-            if(left) {
-                node->left = getNode(child);
+             if(nodetree.count(v[1])==0){
+                TreeNode* child=new TreeNode(v[1]);
+                nodetree[v[1]]=child;
             }
-            else {
-                node->right = getNode(child);   
-            }
-            isChild.insert(child);
+            
+            if(v[2]==1) nodetree[v[0]]->left=nodetree[v[1]];
+            else nodetree[v[0]]->right=nodetree[v[1]];
+            
+            ischild[v[1]]=true;
         }
         
-		// Iterate over map, and get root node
-        for(auto& [node, ptr] : nodeMap) {
-            if(isChild.find(node) == isChild.end()) return ptr;
+        TreeNode *ans=NULL;
+        
+        for(auto &v:a){
+            if(ischild[v[0]]!=true)
+            {
+                ans=nodetree[v[0]];
+                break;
+            }
         }
-        return NULL;
+        
+        return ans;
     }
 };
