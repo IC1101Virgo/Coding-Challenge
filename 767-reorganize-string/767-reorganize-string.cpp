020c1f9538a18edc736878f9aa0792d8fb4cdc6c
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-15 08:02:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.18%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    string reorganizeString(string S) {
+    string reorganizeString(string s) {
         string res="";
-        unordered_map<char,int> mp;
-        priority_queue<pair<int,char>>pq;
         
-        for(auto s: S)
-            mp[s]+=1;
+        unordered_map<char,int> m;
+        priority_queue<pair<int,char>> pq;
         
-        for(auto m: mp)
-            pq.push(make_pair(m.second,m.first));
+        for(auto x:s){
+            m[x]++;
+        }
+        
+        for(auto y:m){
+            pq.push(make_pair(y.second,y.first));
+        }
         
         while(pq.size()>1){
-            auto top1= pq.top();
+            auto top1=pq.top();
             pq.pop();
-            auto top2 = pq.top();
+            auto top2=pq.top();
             pq.pop();
+            
+            top1.first-=1;
+            top2.first-=1;
             
             res+=top1.second;
             res+=top2.second;
             
-            top1.first -=1;
-            top2.first -= 1;
-            
-            if(top1.first > 0)
+            if(top1.first>0)
                 pq.push(top1);
             
-            if(top2.first > 0)
+            if(top2.first>0)
                 pq.push(top2);
         }
         
         if(!pq.empty()){
-            if(pq.top().first > 1)
+            if(pq.top().first>1)
                 return "";
             
-            else
-                res+=pq.top().second;
+            else res+=pq.top().second;
         }
         
         return res;
