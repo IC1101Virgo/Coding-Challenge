85d99045195e7514ec6b4ded89835697f90c8731
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-18 07:54:39 +0530

    Added solution - LeetHub

--- Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
+++ Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
@@ -11,40 +11,43 @@ class Solution{
 public:
     int minThrow(int n, int arr[]){
         // code here
-         unordered_map<int,int>m;
-       for(int i=0;i<2*n;i+=2)
-       {
-          m[arr[i]]=arr[i+1];
-       }
-       vector<int>vis(31,0);
-       queue<int>q;
-       q.push(1);
-       vis[1]=1;
-       int steps=0;
-       while(!q.empty())
-       {
+        
+        unordered_map<int,int>m;
+        
+        for(int i=0;i<2*n;i+=2){
+            m[arr[i]]=arr[i+1];
+        }
+        
+        queue<int> q;
+        vector<int> vis(31,0);
+        
+        q.push(1);
+        vis[1]=1;
+        int steps=0;
+        
+        while(!q.empty()){
            int size=q.size();
-           for(int i=0;i<size;i++)
-           {
-               int x=q.front();
-               q.pop();
-               if(x==30)return steps;
-               for(int k=1;k<=6;k++)
-               {
-                   if(x+k>30)break;
-                   if(!vis[k+x])
-                   {
-                       vis[k+x]=1;
-                       if(m.find(k+x)==m.end())
-                           q.push(k+x);
-                       else
-                           q.push(m[k+x]);
-                   }    
-               }
-           }
-           steps++;
-       }
-       return -1;
+            
+            for(int i=0;i<size;i++){
+                int x=q.front();
+                q.pop();
+                
+                if(x==30) return steps;
+                
+                for(int j=1;j<=6;j++){
+                    if(x+j>30) break;
+                    
+                    if(vis[x+j]==0){
+                        vis[x+j]=1;
+                        if(m.find(x+j)==m.end())
+                        q.push(x+j);
+                        else q.push(m[x+j]);
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
     }
 };
 
