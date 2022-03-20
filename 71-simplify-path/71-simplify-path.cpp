c934333b3df0348ec0b1cad501ee6210f53964c2
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-20 07:19:55 +0530

    Time: 8 ms (62.24%), Space: 9.3 MB (73.34%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     string simplifyPath(string A) {
-        vector<string> v;
-    string ans="";
-    for(int i=0;i<A.length();i++){
-        string path="";
-        while(i<A.length() && A[i]!='/')
-        path+=A[i++];
-        if(path=="" || path==".")
-        continue;
-        else 
-        if(path=="..")
-        {
-            if(v.size()>0)
-            v.pop_back();
+        vector<string>v;
+        int n=A.size();
+        
+        string res;
+        
+        for(int i=0;i<n;i++){
+            string path="";
+            
+            while(i<n && A[i]!='/'){
+                path+=A[i];
+                i++;
+            }
+            
+            if(path=="" || path==".")
+                continue;
+            
+            else
+            if(path=="..")
+            {
+                if(v.size()>0)
+                    v.pop_back();
+            }
+            else
+            v.push_back(path);
         }
-        else v.push_back(path);
-    }
-    if(v.size()==0)
-    return "/";
-    for(int i=0;i<v.size();i++)
-    ans+="/"+v[i];
-    return ans;
+        
+        for(int i=0;i<v.size();i++)
+            res+="/"+v[i];
+        
+        if(res.size()==0) return "/"; 
+            return res;
     }
 };
