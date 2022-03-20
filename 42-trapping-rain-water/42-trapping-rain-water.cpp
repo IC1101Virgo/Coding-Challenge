1939dfba54b9e0142167876314e77f1e4b2a78ec
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-20 11:37:33 +0530

    Time: 17 ms (48.86%), Space: 17.8 MB (45.52%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int n=h.size();
+    int trap(vector<int>& A) {
+        int n=A.size();
         
-        vector<int> l(n), r(n);
+        int i=0, j=n-1;
+        if(n<=2)
+            return 0;
         
-        int lm=h[0];
-       
+        int lm=A[i],rm=A[j];
+        int trapwater=0;
         
-        for(int i=0;i<n;i++){
-            l[i]=lm;
-            lm=max(lm,h[i]);
+        while(i<j){
+            if(A[i]<A[j]){
+                lm=max(lm,A[++i]);
+                    trapwater+=lm-A[i];
+              
+            }
+            
+            else{
+                rm=max(rm,A[--j]);
+                trapwater+=rm-A[j];
+              
+            }
         }
         
-        int rm=h[n-1];
-       
-        
-        for(int i=n-1;i>=0;i--){
-            r[i]=rm;
-            rm=max(rm,h[i]);
-        }
-        
-        int water=0;
-        
-        for(int i=1;i<n-1;i++){
-            if(h[i]<l[i] && h[i]<r[i])
-                water+=min(l[i],r[i])-h[i];
-        }
-        
-        return water;
+        return trapwater;
     }
 };
