c766376573bbb1a39c6aae5768e046806467ffe7
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-03-23 20:50:07 +0530

    Time: 3 ms (53.77%), Space: 6.3 MB (46.60%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.length();
+        
+        vector<int> dp(n+1,0);
+        
+        dp[n]=1;
+        
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!='0'){
+                dp[i]=dp[i+1];
+                
+                if(i<n-1 && (s[i]=='1' || s[i]== '2' && s[i+1]<= '6'))
+                    dp[i]+=dp[i+2];
+            }
+        }
+        
+        return dp[0];
+    }
+};
