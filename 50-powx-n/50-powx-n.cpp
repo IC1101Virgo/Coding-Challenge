37e2b6ac141e1c53faea561f81dae0d98575baf0
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-11 18:45:56 +0530

    Time: 2 ms (45.49%), Space: 5.9 MB (81.24%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, long n) {
+        if(n==0)
+            return 1;
+        if(x==0)
+            return 0;
+        if(n==1)
+            return x;
+        
+        if(n<0)
+            return myPow(1/x,-n);
+        double res= myPow(x*x,n/2);
+        
+        if(n%2!=0)
+            res*=x;
+        
+        return res;
+    }
+};
