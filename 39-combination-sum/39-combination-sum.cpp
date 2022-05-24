9d326172bf9c282d4a42d107e00bc2528ea0138d
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-24 09:09:56 +0530

    Time: 1145 ms (5.01%), Space: 109.5 MB (5.01%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    void helper(vector<vector<int>> &res, vector<int> v,  vector<int> &A, int B){
-    int n=A.size();
-    
-    if(B<0)
-    return;
-    
-    if(B==0){
-        sort(v.begin(),v.end());
-        for(auto x:res){
-            if(x==v)
+    void helper(vector<int> &A, int B, vector<vector<int>> &res, vector<int> v){
+        if(B<0)
             return;
+        
+        if(B==0){
+            sort(v.begin(),v.end());
+            for(auto x:res){
+                if(x==v)
+                    return;
+                }
+            res.push_back(v);
         }
         
-        res.push_back(v);
-    }
-    
-    for(int i=0;i<n;i++){
-        if(B-A[i]>=0){
-            v.push_back(A[i]);
-            helper(res,v,A,B-A[i]);
-            v.pop_back();
+        for(int i=0;i<A.size();i++){
+            if(B-A[i]>=0){
+                v.push_back(A[i]);
+                helper(A,B-A[i],res,v);
+                v.pop_back();
+            }
         }
     }
-    
-    return;
-}
     vector<vector<int>> combinationSum(vector<int>& A, int B) {
         sort(A.begin(),A.end());
-    vector<vector<int>> res;
-    vector<int> v;
-    helper(res,v,A,B);
-    sort(res.begin(),res.end());
-    return res;
+        vector<vector<int>> res;
+        vector<int> v;
+        helper(A,B,res,v);
+        return res;
     }
 };
