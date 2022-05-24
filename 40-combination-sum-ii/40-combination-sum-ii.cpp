b0c307df206fba5d271d8951dd9e3e6715d51d7f
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-24 11:19:04 +0530

    Time: 35 ms (14.42%), Space: 16.2 MB (14.47%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    
-    void helper(vector<int> &can, int t, vector<int> v, vector<vector<int>> & res, int ind){
-        if(t<0)
+   void helper(vector<int>& can, int t,vector<int> v,int in,int sum, vector<vector<int>> &res){
+        if(sum>t)
             return;
         
-        if(t==0)
-        {
+        if(sum==t){
             res.push_back(v);
             return;
         }
         
-        for(int i=ind;i<can.size();i++){
-            if(i>ind && can[i]==can[i-1])
+        for(int i=in;i<can.size();i++){
+            if(i!=in && can[i]==can[i-1])
                 continue;
-                v.push_back(can[i]);
-                helper(can,t-can[i],v,res,i+1);
-                v.pop_back();
             
+            v.push_back(can[i]);
+            sum += can[i];
+            helper(can, t, v, i+1, sum, res);
+            sum -= can[i];
+            v.pop_back();
         }
         
         return;
     }
     vector<vector<int>> combinationSum2(vector<int>& can, int t) {
         sort(can.begin(),can.end());
+       vector<int> v;
         vector<vector<int>> res;
-        vector<int> v;
         
-        helper(can,t,v,res,0);
+        helper(can,t,v,0,0,res);
         
         return res;
     }
