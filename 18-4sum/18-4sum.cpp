c91d88ab122f0fbde1e44ee7504b10c4f55edb9e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-26 09:04:39 +0530

    Time: 431 ms (16.04%), Space: 49.3 MB (15.96%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        set<vector<int>> st;
+        
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int x=j+1, y=n-1;
+                int tar=target-nums[i]-nums[j];
+                while(x<y){
+                    if(nums[x]+nums[y]>tar) y--;
+                    
+                    else if(nums[x]+nums[y]<tar) x++;
+                    
+                    else{
+                        st.insert({nums[i],nums[j],nums[x],nums[y]});
+                        x++;
+                        y--;
+                    }
+                }
+            }
+        }
+        
+        for(auto x:st){
+            res.push_back(x);
+        }
+        
+        return res;
+    }
+};
