497ebe0e65cc08ddc106b7d670c2e2d8bc09d11a
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-27 08:16:06 +0530

    Time: 34 ms (44.53%), Space: 21.8 MB (7.12%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<int>> row(9,vector<int>(9,0));
-            vector<vector<int>> col(9,vector<int>(9,0));
-            vector<vector<int>> box(9,vector<int>(9,0));
-        
+              
+        vector<unordered_set<int>> row(9, unordered_set<int>());
+	vector<unordered_set<int>> col(9, unordered_set<int>());
+	vector<unordered_set<int>> boxes(9, unordered_set<int>());
+
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                    int num=board[i][j]-'1';
-                    int k=i/3*3+j/3;
-                    if(row[num][j]++||col[i][num]++||box[k][num]++)
-                        return false;
-                }
+               if (board[i][j] == '.')
+		  continue;
+
+		int num = board[i][j]-'1' ;
+
+		if (row[i].count(num))
+		  return false;
+
+		if (col[j].count(num))
+		  return false;
+
+		// This is a straight-forward math formula to locate a box
+		// Out of the 9 boxes
+		int box = (i / 3) * 3 + j / 3;
+
+		if (boxes[box].count(num))
+		  return false;
+
+		row[i].insert(num);
+		col[j].insert(num);
+		boxes[box].insert(num);
+                
             }
         }
         
