de269f8c033265dd704cbb536b8dfeacf166ae38
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-05-31 18:26:26 +0530

    Time: 21 ms (29.12%), Space: 8.3 MB (76.85%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,24 +1,17 @@
-bool compare(pair <int, char> p, pair <int, char> q){
-    return p.first > q.first;
-}
 class Solution {
 public:
     string frequencySort(string s) {
-        map <char, int> m;
-        for (int i = 0; i < s.length(); i++) {
-            m[s[i]]++;
+          unordered_map<char,int> freq;           //for frequency of characters
+        priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); //heap will be constructed on the basis of frequency
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second); //frequency times the character
+            maxheap.pop();
         }
-        vector < pair <int, char> > vec;
-        for (auto i : m) {
-            vec.push_back({i.second, i.first});
-        }
-        sort(vec.begin(), vec.end(), &compare);
-        string str = "";
-        for (auto i : vec){
-            // cout<<i.first<<' '<<i.second<<endl;
-            char ch = i.second;
-            while (i.first--) str += ch;
-        }
-        return str;
+        return s;
     }
 };
