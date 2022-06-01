f8a436e8155cb220bff9785dfe1aef3367fae43a
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-01 21:28:42 +0530

    Time: 12 ms (45.00%), Space: 7 MB (61.38%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -9,12 +9,13 @@ public:
         
        
         
-         for(int j=1;j<=m;j++){
+        for(int j=1;j<=m;j++){
             if(p[j-1]=='*')
                 dp[0][j]=dp[0][j-2];
             
             else dp[0][j]=0;
         } 
+        
         
         for(int i=1; i<=n;i++){
             for(int j=1;j<=m;j++){
