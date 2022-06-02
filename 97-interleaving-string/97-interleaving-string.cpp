1524a56d28b1c24d0818700a7fbbb95db90f1d33
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-02 10:30:53 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (76.02%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-         int n1 = s1.size();
-        int n2 = s2.size();
-        int n3 = s3.size();
-        
-        if(n3 == 0) return true;
-        
-        if(n1+n2 != n3) return false; 
+        int n1=s1.size();
+        int n2=s2.size();
+        int n3=s3.size();
         
         int dp[n1+1][n2+1];
-        memset(dp, 0, sizeof(dp));
+        memset(dp,0,sizeof(dp));
         
-        for(int i=0; i<=n1; i++){
-            for(int j=0; j<=n2; j++){
-                if(i == 0 && j == 0) //both strings are empty so it is interleaving
-                    dp[i][j] = 1;
+        if(n3==0)
+            return 1;
+        
+        if(n1+n2!=n3)
+            return 0;
+        
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=1;
                 
-                else if(i == 0){ //s1 is empty
-                    if(s2[j-1] == s3[j-1])
-                        dp[i][j] = dp[i][j-1];
+               else if(i==0){
+                    if(s2[j-1]==s3[j-1])
+                        dp[i][j]=dp[i][j-1];
                 }
                 
-                else if(j == 0){ // s2 is empty
-                    if(s1[i-1] == s3[i-1])
-                        dp[i][j] = dp[i-1][j];
+                else if(j==0){
+                    if(s1[i-1]==s3[i-1])
+                        dp[i][j]=dp[i-1][j];
                 }
                 
-                else if(s1[i-1] != s3[i+j-1] && s2[j-1] == s3[i+j-1]) //if not match with s1
-                    dp[i][j] = dp[i][j-1];
+                else if(s1[i-1]!=s3[i+j-1] && s2[j-1]==s3[i+j-1])
+                    dp[i][j]=dp[i][j-1];
                 
-                else if(s1[i-1] == s3[i+j-1] && s2[j-1] != s3[i+j-1])   //if not match with s2
-                    dp[i][j] = dp[i-1][j];
+                else if(s1[i-1]==s3[i+j-1] && s2[j-1]!=s3[i+j-1])
+                    dp[i][j]=dp[i-1][j];
                 
-                else if(s1[i-1] == s3[i+j-1] && s2[j-1] == s3[i+j-1])  // If match with both s1 and s2
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
             }
-        }      
+        }
+        
         return dp[n1][n2];
     }
 };
