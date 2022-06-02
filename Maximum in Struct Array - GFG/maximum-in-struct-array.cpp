19e82095070e2b7efcab8441ea482285cea9f907
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-02 11:54:44 +0530

    Added solution - LeetHub

---
+++ Maximum in Struct Array - GFG/maximum-in-struct-array.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Height {
+	int feet;
+	int inches;
+};
+
+int findMax(Height arr[], int n);
+
+// driver program
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n, tmp1, tmp2;
+	    cin>>n;
+	    Height arr[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>tmp1>>tmp2;
+	        arr[i].feet = tmp1;
+	        arr[i].inches = tmp2;
+	    }
+	    int res = findMax(arr, n);
+	    cout<<res<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*
+Structure of the element of the array is as
+struct Height {
+	int feet;
+	int inches;
+};
+*/
+// function must return the maximum Height
+// return the height in inches
+int findMax(Height arr[], int n)
+{
+    // Code here
+    int ans=0;
+    
+    for(int i=0;i<n;i++){
+        ans=max(ans,(arr[i].feet*12)+arr[i].inches);
+    }
+    
+    return ans;
+}
