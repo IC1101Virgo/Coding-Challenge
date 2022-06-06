44cae5889eee1841fb6d1d1660ecdac2073460f6
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-06 16:37:52 +0530

    Time: 159 ms (29.39%), Space: 29.6 MB (41.23%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    int helper(int k,int n, vector<vector<int>> &memo){
-        if(k==1)
-            return n;
+   
+       int helper(int k, int n, vector<vector<int>> &memo){
+    if(k==1)
+    return n;
+    
+    if(n==0||n==1)
+    return n;
+    
+    if(memo[k][n]!=-1) return memo[k][n];
+    
+    int temp=0, lo=0,hi=n, mn=INT_MAX;
+    
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
         
-        if(n==0||n==1)
-            return n;
+        int left=helper(k-1,mid-1,memo);
+        int right=helper(k,n-mid,memo);
         
-        if(memo[k][n]!=-1) return memo[k][n];
+        temp=1+max(left,right);
         
-        int hi=n,lo=0,temp=0, mn=INT_MAX;
+        if(left<right)
+        lo=mid+1;
         
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            
-            int left=helper(k-1,mid-1,memo);
-            int right=helper(k,n-mid,memo);
-            
-            temp=1+max(left,right);
-            
-            if(left<right)
-                lo=mid+1;
-            
-            else hi=mid-1;
-            
-            mn=min(mn,temp);
-        }
+        else hi=mid-1;
         
-        return memo[k][n]=mn;
+        mn=min(mn,temp);
     }
+    
+    return memo[k][n]=mn;
+}
     int superEggDrop(int k, int n) {
-        vector<vector<int>> memo(k+1,vector<int>(n+1,-1));
-        
-        return helper(k,n,memo);
+         vector<vector<int>> memo(k+1,vector<int>(n+1,-1));
+    return helper(k,n,memo);
     }
 };
