129eea10eaf67290d47baecd3b487a5226ba7c18
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-06 10:07:12 +0530

    Time: 165 ms (5.04%), Space: 50.4 MB (5.54%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,60 +1,68 @@
 class Solution {
 public:
-    bool isSafe(int r, int c, vector<string>b){
-        int i=r, j = c;
-        int n= b.size();
+    bool issafe(int r,int c, vector<string> v){
+        int i=r,j=c;
+        int n=v.size();
+        
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!= r  && b[i][j] == 'Q')return 0;
+            if(i!=r && v[i][j]=='Q') return 0;
             i++;
         }
-        i=r,j=c;
+        
+         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!=r  && b[i][j] == 'Q')return 0;
+            if(i!=r && v[i][j]=='Q') return 0;
             i--;
         }
+        
         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if( j!=c && b[i][j] == 'Q')return 0;
-            j++;
-        }
-        i=r,j=c;
-        while(i>=0 && i<n && j>=0 && j<n){
-            if( j!=c && b[i][j] == 'Q')return 0;
+            if(j!=c && v[i][j]=='Q') return 0;
             j--;
         }
+        
         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!= r && j!=c && b[i][j] == 'Q')return 0;
-            i++;j++;
+            if(j!=c && v[i][j]=='Q') return 0;
+            j++;
         }
-        i=r,j=c;
+        
+         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!= r && j!=c && b[i][j] == 'Q')return 0;
-            i--;j--;
+            if(i!=r && j!=c && v[i][j]=='Q') return 0;
+            i++; j--;
         }
+        
         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!= r && j!=c && b[i][j] == 'Q')return 0;
-            i++;j--;
+            if(i!=r && j!=c && v[i][j]=='Q') return 0;
+            i++; j++;
         }
-        i=r,j=c;
+        
+         i=r,j=c;
         while(i>=0 && i<n && j>=0 && j<n){
-            if(i!= r && j!=c && b[i][j] == 'Q')return 0;
+            if(i!=r && j!=c && v[i][j]=='Q') return 0;
             i--; j++;
         }
+        
+        i=r,j=c;
+        while(i>=0 && i<n && j>=0 && j<n){
+            if(i!=r && j!=c && v[i][j]=='Q') return 0;
+            i--; j--;
+        }
+        
         return 1;
     }
-    
-    void issafe(int n, vector<vector<string>> &res, vector<string> v, int row){
+    void helper(int n,  vector<vector<string>> &res, vector<string> v, int row){
         if(row==n){
             res.push_back(v);
             return;
         }
         
         for(int col=0;col<n;col++){
-            if(isSafe(row,col,v)){
+            if(issafe(row,col,v)){
                 v[row][col]='Q';
-                issafe(n,res,v,row+1);
+                helper(n,res,v,row+1);
                 v[row][col]='.';
             }
         }
@@ -63,8 +71,7 @@ public:
         vector<vector<string>> res;
         vector<string> v(n,string(n,'.'));
         
-        issafe(n,res,v,0);
-        
+        helper(n,res,v,0);
         return res;
     }
 };
