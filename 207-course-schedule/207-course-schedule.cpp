cc0796b75306c9707c6c2c7fb188f9c19b65eed0
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-13 22:34:00 +0530

    Time: 50 ms (15.40%), Space: 14.2 MB (46.90%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    bool iscycle(vector<int> adj[],vector<int> &vis, int id ){
-        if(vis[id]==1)
-            return true;
+   bool dfs(vector<int> &vis, vector<int> &dfsvis, vector<int> adj[], int id){
+        vis[id]=1;
+        dfsvis[id]=1;
         
-        if(vis[id]==0){
-            vis[id]=1;
-            
-            for(auto edge:adj[id])
-            {
-                if(iscycle(adj,vis,edge))
-                    return true;
+        for(auto edge:adj[id]){
+            if(vis[edge]==-1){
+                if(dfs(vis,dfsvis,adj,edge))
+                return true;
             }
+            
+            if(dfsvis[edge]==1)
+            return true;
         }
         
-        vis[id]=2;
+        dfsvis[id]=-1;
         return false;
     }
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> adj[n];
+    
+    bool canFinish(int v, vector<vector<int>>& p) {
+       vector<int> adj[v];
         
-        for(auto edge:pre)
-            adj[edge[1]].push_back(edge[0]);
+        for(auto x:p){
+            adj[x[0]].push_back(x[1]);
+        }
+       vector<int> vis(v,-1);
+        vector<int> dfsvis(v,-1);
         
-        vector<int> vis(n,0);
-        
-        for(int i=0;i<n;i++)
-        {
-            if(iscycle(adj,vis,i))
+        for(int i=0;i<v;i++){
+            if(vis[i]==-1){
+                if(dfs(vis,dfsvis,adj,i))
                 return false;
+            }
         }
         
         return true;
