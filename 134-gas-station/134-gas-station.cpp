f373bfa90c62b0b37c21687df9d3006d8923ed40
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-15 17:10:09 +0530

    Time: 189 ms (5.10%), Space: 69.5 MB (13.04%) - LeetHub

---
+++ 134-gas-station/134-gas-station.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& A, vector<int>& B) {
+        int n=A.size();
+    
+    int have=0,give=0;
+    
+    for(int i=0;i<n;i++){
+        have+=A[i];
+        give+=B[i];
+    }
+    
+    if(have<give)
+    return -1;
+    
+    int curr=0,start=0;
+    for(int i=0;i<n;i++){
+        if(curr<=0){
+            start=i;
+            curr=0;
+        }
+        
+        curr+=A[i]-B[i];
+    }
+    
+    return start;
+    }
+};
