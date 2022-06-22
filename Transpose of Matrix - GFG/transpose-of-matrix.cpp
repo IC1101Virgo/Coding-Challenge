fc6006cbc4e764c1f2fcfceae527bfb684688fbc
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-22 18:42:21 +0530

    Added solution - LeetHub

---
+++ Transpose of Matrix - GFG/transpose-of-matrix.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& mat, int n)
+    { 
+        // code here 
+         
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+               
+                swap(mat[j][i],mat[i][j]);
+            }
+        }
+        
+        return;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.transpose(matrix,n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
