ac717a559bf5eb1a82c4df89955ab4870c067742
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-22 18:40:43 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
@@ -20,7 +20,7 @@ class Solution
         }
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+            for(int j=i;j<n;j++){
                
                 swap(mat[j][i],mat[i][j]);
             }
