38ca6793e909f38d48ba449d974b438d7cb72fdb
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-28 09:03:48 +0530

    Time: 10 ms (50.61%), Space: 8.9 MB (50.72%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+    Node* cloneGraph(Node* node) {
+        if(!node)
+            return NULL;
+        
+        if(mp[node])
+            return mp[node];
+        
+        Node *clone=new Node(node->val);
+        mp[node]=clone;
+        
+        for(auto curr:node->neighbors){
+            (clone->neighbors).push_back(cloneGraph(curr));
+        
+        }
+        
+        return clone;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
