d6d79f6b987dbfcd41695bd9e4c3bc9b8407eca3
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-29 09:27:18 +0530

    Time: 15 ms (46.63%), Space: 11.1 MB (74.28%) - LeetHub

---
+++ 179-largest-number/179-largest-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool compare(int a, int b){
+        string x=to_string(a);
+        string y=to_string(b);
+        
+        return (x+y)>(y+x);
+
+    }
+    string largestNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        string s="";
+        
+        sort(nums.begin(),nums.end(),compare);
+        
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+            s+=to_string(nums[i]);
+            
+            if(nums[i]==0)
+                count++;
+        }
+        
+        return count==n? "0":s;
+    }
+};
