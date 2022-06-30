b1ff6f12c7dd1469df0462435fbe097e26609172
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-06-30 17:08:15 +0530

    Time: 62 ms (8.14%), Space: 19.9 MB (40.21%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-              
-        vector<unordered_set<int>> row(9, unordered_set<int>());
-	vector<unordered_set<int>> col(9, unordered_set<int>());
-	vector<unordered_set<int>> boxes(9, unordered_set<int>());
-
+       
+        vector<vector<int>> row(9,vector<int>(9,0));
+        vector<vector<int>> col(9,vector<int>(9,0));
+        vector<vector<int>> box(9,vector<int>(9,0));
+        
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-               if (board[i][j] == '.')
-		  continue;
-
-		int num = board[i][j]-'1' ;
-
-		if (row[i].count(num))
-		  return false;
-
-		if (col[j].count(num))
-		  return false;
-
-		// This is a straight-forward math formula to locate a box
-		// Out of the 9 boxes
-		int box = (i / 3) * 3 + j / 3;
-
-		if (boxes[box].count(num))
-		  return false;
-
-		row[i].insert(num);
-		col[j].insert(num);
-		boxes[box].insert(num);
+                 
                 
+                if(board[i][j]!='.'){
+                    int num=board[i][j]-'1';
+                    
+                    int x=i/3*3+j/3;
+                    
+                    if(row[i][num]++ || col[num][j]++ || box[x][num]++)
+                        return false;
+                }
             }
         }
         
