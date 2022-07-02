e4bb843b7a3c4b6671794a0c279aca2895581c99
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-02 08:55:22 +0530

    Time: 44 ms (23.97%), Space: 9 MB (37.86%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -5,18 +5,16 @@ public:
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                dp[i][0]=i;
                 dp[0][j]=j;
+                dp[i][0]=i;
             }
         }
         
-       
-        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
+                
                 if(w1[i-1]==w2[j-1])
                     dp[i][j]=dp[i-1][j-1];
                 
