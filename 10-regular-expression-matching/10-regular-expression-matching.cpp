720e617ccd6edbe2f2019ef423f617d616ac935a
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-02 18:09:57 +0530

    Time: 4 ms (81.63%), Space: 7 MB (62.42%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-       int n=s.size(), m=p.size();
+        int n=s.size(), m=p.size();
         
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
         
         dp[0][0]=1;
         
-        for(int j=1;j<=m;j++){
-            if(p[j-1]=='*')
-                dp[0][j]=dp[0][j-2];
+        for(int i=1;i<=m;i++){
+            if(p[i-1]=='*')
+                dp[0][i]=dp[0][i-2];
             
-            else dp[0][j]=0;
+            else dp[0][i]=0;
         }
         
         for(int i=1;i<=n;i++){
@@ -19,11 +19,11 @@ public:
                 if(p[j-1]=='*'){
                     dp[i][j]=dp[i][j-2];
                     
-                    if(s[i-1]==p[j-2] || p[j-2]=='.')
-                        dp[i][j]=dp[i][j]||dp[i-1][j];
+                    if(s[i-1]==p[j-2]||p[j-2]=='.')
+                        dp[i][j]=max(dp[i][j],dp[i-1][j]);
                 }
                 
-                else if(s[i-1]==p[j-1] || p[j-1]=='.')
+                else if(s[i-1]==p[j-1]||p[j-1]=='.')
                     dp[i][j]=dp[i-1][j-1];
                 
                 else dp[i][j]=0;
