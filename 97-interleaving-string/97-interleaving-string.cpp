e57c3d315105feb301168ab668a0ec8aea9da0db
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-02 18:27:59 +0530

    Time: 3 ms (84.64%), Space: 6.7 MB (57.57%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n1=s1.size();
-        int n2=s2.size();
-        int n3=s3.size();
+        int n1=s1.size(), n2=s2.size(), n3=s3.size();
         
-        int dp[n1+1][n2+1];
-        memset(dp,0,sizeof(dp));
+        vector<vector<bool>> dp(n1+1,vector<bool>(n2+1,0));
         
         if(n3==0)
-            return 1;
+            return true;
         
-        if(n1+n2!=n3)
-            return 0;
+        if( n1+n2!=n3)
+            return false;
         
         for(int i=0;i<=n1;i++){
             for(int j=0;j<=n2;j++){
                 if(i==0 && j==0)
                     dp[i][j]=1;
                 
-               else if(i==0){
+                else if(i==0){
                     if(s2[j-1]==s3[j-1])
                         dp[i][j]=dp[i][j-1];
                 }
@@ -32,11 +29,11 @@ public:
                 else if(s1[i-1]!=s3[i+j-1] && s2[j-1]==s3[i+j-1])
                     dp[i][j]=dp[i][j-1];
                 
-                else if(s1[i-1]==s3[i+j-1] && s2[j-1]!=s3[i+j-1])
+                 else if(s1[i-1]==s3[i+j-1] && s2[j-1]!=s3[i+j-1])
                     dp[i][j]=dp[i-1][j];
                 
-                else if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
-                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                 else if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
+                    dp[i][j]=dp[i][j-1]||dp[i-1][j];
             }
         }
         
