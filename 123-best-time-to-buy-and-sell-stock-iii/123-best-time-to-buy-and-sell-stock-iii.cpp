0f5bb7dd5e05166bd1255f9c8bdd8a984b3be23d
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-04 20:41:55 +0530

    Time: 269 ms (59.21%), Space: 75.2 MB (84.60%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int firstbuy=INT_MAX, secondbuy=INT_MAX, fp=0,sp=0;
+    int maxProfit(vector<int>& p) {
+        int n=p.size();
         
-        for(auto cp:prices){
-            firstbuy=min(firstbuy,cp);
-            fp=max(fp,cp-firstbuy);
-            secondbuy=min(secondbuy, cp-fp);
-            sp=max(sp,cp-secondbuy);
+        int fb=INT_MAX,fp=INT_MIN, sb=INT_MAX, sp=INT_MIN;
+        
+        for(int i=0;i<n;i++){
+            fb=min(fb,p[i]);
+            fp=max(fp,p[i]-fb);
+            sb=min(sb,p[i]-fp);
+            sp=max(sp,p[i]-sb);
         }
         
         return sp;
