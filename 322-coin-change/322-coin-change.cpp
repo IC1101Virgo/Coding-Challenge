639f8dddbe25542fdd9380891ddeda728b1090c9
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-05 11:56:32 +0530

    Time: 105 ms (69.80%), Space: 11.5 MB (75.09%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[13][10002];
+    
+    int helper(vector<int>& coins, int n, int amount){
+        if(n<=0)
+            return dp[n][amount]=INT_MAX-1;
+        
+        if(amount==0)
+        return dp[n][amount]=0;
+        
+        if(dp[n][amount]!=-1)
+            return dp[n][amount];
+        
+        if(coins[n-1]<=amount)
+            return dp[n][amount]=min(1+helper(coins,n,amount-coins[n-1]), helper(coins,n-1,amount));
+        
+        else return dp[n][amount]=helper(coins,n-1,amount);
+        
+    }
+        
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        memset(dp,-1,sizeof(dp));
+        int ans= helper(coins, n, amount);
+        
+        return ans==INT_MAX-1?-1:ans;
+    }
+};
