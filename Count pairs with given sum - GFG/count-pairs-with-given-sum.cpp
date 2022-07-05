5e7a4d0d705f20b5d45d1d444d06412515654ba5
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-05 10:04:48 +0530

    Added solution - LeetHub

---
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+      unordered_map<int, int> m;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int val = k - arr[i];        // int val = sum - arr[i]
+            if(m[val]){
+                ans += m[val];
+            }
+            m[arr[i]]++;
+        }
+        return ans;    
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}  // } Driver Code Ends
