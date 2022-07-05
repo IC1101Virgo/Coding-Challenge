2be9564d8824e1069d6415487ce58a9c201c85ef
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-05 21:46:16 +0530

    Added solution - LeetHub

--- Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -7,8 +7,9 @@ using namespace std;
 //Function to find the maximum possible amount of money we can win.
 class Solution{
     public:
-    typedef long long ll;
-    int helper(int arr[],int i, int j,vector<vector<int>> &dp){
+    long long helper(int arr[], int i, int j, vector<vector<long long>> &dp ){
+       
+        
         if(dp[i][j]!=-1)
         return dp[i][j];
         
@@ -18,12 +19,15 @@ class Solution{
         else {
             int a=arr[i]+min(helper(arr,i+1,j-1,dp),helper(arr,i+2,j,dp));
             int b=arr[j]+min(helper(arr,i,j-2,dp),helper(arr,i+1,j-1,dp));
+            
             return dp[i][j]=max(a,b);
         }
+        
     }
     long long maximumAmount(int arr[], int n){
         // Your code here
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<long long>> dp(n,vector<long long>(n,-1));
+        
         return helper(arr,0,n-1,dp);
     }
 };
