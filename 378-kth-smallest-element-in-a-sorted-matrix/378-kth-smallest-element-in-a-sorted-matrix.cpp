f15cf32711839876016bb0b08e3ba760373aa8a1
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-06 10:10:07 +0530

    Time: 33 ms (89.63%), Space: 13.1 MB (93.06%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int solve(vector<vector<int>>& mat, int mid){
-       int count = 0, n = mat.size(), i = n-1, j = 0;
-        while(i >= 0 && j < n){
-            if(mat[i][j] > mid) i--;
+        int n=mat.size(), i=n-1,j=0;
+        
+        int count=0;
+        
+        while(i>=0 && j<n){
+            if(mat[i][j]>mid) i--;
+            
             else{
-                count += (i+1);
+                count+=(i+1);
                 j++;
             }
         }
+        
         return count;
     }
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n=matrix.size();
-        int lo=matrix[0][0], hi=matrix[n-1][n-1];
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+        int n=mat.size();
+        
+        int lo=mat[0][0], hi=mat[n-1][n-1];
         
         while(lo<hi){
             int mid=lo+(hi-lo)/2;
             
-            int pos=solve(matrix,mid);
+            int pos=solve(mat,mid);
             
             if(pos<k) lo=mid+1;
             
