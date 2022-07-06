bfb61683f66b35948da2d5bc9ce63b0fafe32753
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-06 10:20:00 +0530

    Time: 261 ms (22.29%), Space: 14.8 MB (88.24%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int t) {
+         int n=mat.size(),m=mat[0].size(), i=0,j=m-1;
+        
+        int count=0;
+        
+        while(i<n && j>=0){
+            if(mat[i][j]==t)
+                return 1;
+            
+            else if(mat[i][j]>t){
+                j--;
+            }
+            
+            else{
+               
+                i++;
+            }
+        }
+        
+        return 0;
+        
+    }
+};
