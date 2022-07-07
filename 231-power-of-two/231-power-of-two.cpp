f9419641913740fef4776a325f575585299541ed
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-07 09:57:27 +0530

    Time: 3 ms (58.75%), Space: 5.9 MB (70.02%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0)
+        if(n<=0)
             return 0;
-        if(floor(log2(n))==ceil(log2(n)))
+        
+        if((n&n-1)==0)
             return 1;
-        else
-            return 0;
+        
+        else return 0;
     }
 };
