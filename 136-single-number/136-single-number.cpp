138cd4ea1054839fc5379446863ef628d27b9bf5
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-07 08:51:56 +0530

    Time: 27 ms (65.10%), Space: 16.9 MB (51.81%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            ans^=nums[i];
+        }
+        
+        return ans;
+    }
+};
