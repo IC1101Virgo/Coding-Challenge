c5e4a791fc140b1763cff18f514c9683b5dab27f
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-10 10:04:31 +0530

    Time: 690 ms (44.33%), Space: 174.1 MB (45.57%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,38 +1,65 @@
 class LRUCache {
 public:
-    list<pair<int,int>> l;
-    unordered_map<int,list<pair<int,int>>::iterator>m;
-    int size;
+     list<pair<int, int>> cache;
+    // this unordered_map will have the key and address of list where that key is stored
+    unordered_map<int, list<pair<int,int>> :: iterator> myMap;
+    int maxSize; // max Capacity
+    
+    // this is the main logic and game changing function
+    // this function will remove the (key, value) from the cache
+    // insert the new (key,value) in front of cache
+    // update/enter the address of (key,address) in map
+    void refreshPosition(int key, int value) {
+        
+        // find the position of the key and remove it from the cache
+        cache.erase(myMap[key]);
+        // push in front the new (key,value) pair
+        cache.push_front(make_pair(key, value));
+        // make entry in map as cache.begin() because we added it in front
+        myMap[key] = cache.begin();
+        
+    }
+   
     LRUCache(int capacity) {
-        size=capacity;
+        maxSize = capacity;
     }
     
+    // check if that value is already present in map
+    // if yes, then refresh the position to make it the first element in cache
+    // return the value from the pair (key, value)
+    // return -1 if not found
     int get(int key) {
-        if(m.find(key)==m.end())
-            return -1;
-        
-     l.splice(l.begin(),l,m[key]);
-        return m[key]->second;
-        
+        // if found in map
+        if(myMap.find(key) != myMap.end()) {
+            // *myMap[key] -> second will give us the corresponding value of the key
+            // because myMap[key] stores the address of the pair and not the pair itself
+            int value = (*myMap[key]).second;
+            refreshPosition(key, value);
+            return value;
+        }
+        return -1; // if not found
     }
     
+    // if a pair is already present then just refresh it
+    // check if that key exists in map, if it does then just refresh the position
     void put(int key, int value) {
-        if(m.find(key)!=m.end()){
-            l.splice(l.begin(),l,m[key]);
-            m[key]->second=value;
-            return;
+        if(myMap.find(key) != myMap.end())
+            refreshPosition(key, value); 
+        // here we refresh (key, value) and not (key, (*myMap[key]).second) because if put [2,1] and put [2,2] is called
+        // then get(2) should give ans 2 and not 1, so refresh with latest value and same key
+        else {
+            // push it in front of the cache
+            cache.push_front(make_pair(key, value));
+            myMap[key] = cache.begin();
+            
+            // if capacity is overflown after this insertion then remove the last element from cache
+            if(myMap.size() > maxSize) {
+                // erase the last element of the cache from map
+                myMap.erase(cache.back().first);
+                // pop that element from cache as well
+                cache.pop_back();
+            }
         }
-        
-        if(l.size()==size){
-             auto d_key=l.back().first;
-                l.pop_back();
-                m.erase(d_key);
-        }
-        
-        l.push_front({key,value});
-        m[key]=l.begin();
-        
-        
     }
 };
 
