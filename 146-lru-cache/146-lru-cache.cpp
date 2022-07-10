9d3b8bd3d3e2ea3b6151600c7d8ebac7beb94729
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-10 08:41:55 +0530

    Time: 491 ms (89.19%), Space: 165.1 MB (71.41%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,70 +1,38 @@
 class LRUCache {
 public:
-    #include<list>
-unordered_map<int, list<int>::iterator> um_dll;
-unordered_map<int, int> key_val;
-int cap;
-int curr;
-list<int> dll;
-
-void delete_key(int key)
-{
-    if(key_val.find(key) == key_val.end())
-        return;
-    dll.erase(um_dll[key]);
-    --curr;
-    key_val.erase(key);
-}
-
-void insert_key_val(int key, int value)
-{
-    key_val[key] = value;
-    dll.push_front(key);
-    um_dll[key] = dll.begin();
-    ++curr;
-}
-
+    list<pair<int,int>> l;
+    unordered_map<int,list<pair<int,int>>::iterator>m;
+    int size;
     LRUCache(int capacity) {
-        um_dll.clear();
-    key_val.clear();
-    dll.clear();
-    cap = capacity;
-    curr = 0;
+        size=capacity;
     }
     
     int get(int key) {
-        if(key_val.find(key) == key_val.end())
-    {
-        return -1;
-    }
-    else
-    {
-        int val = key_val[key];
-        delete_key(key);
-        insert_key_val(key, val);
-        return val;
-    }
+        if(m.find(key)==m.end())
+            return -1;
+        
+     l.splice(l.begin(),l,m[key]);
+        return m[key]->second;
+        
     }
     
     void put(int key, int value) {
-        if(key_val.find(key) != key_val.end())
-    {
-        delete_key(key);
-        insert_key_val(key, value);
-    }
-    else
-    {
-        if(curr == cap)
-        {
-            int least_used = dll.back();
-            delete_key(least_used);
-            insert_key_val(key, value);
+        if(m.find(key)!=m.end()){
+            l.splice(l.begin(),l,m[key]);
+            m[key]->second=value;
+            return;
         }
-        else
-        {
-            insert_key_val(key, value);
+        
+        if(l.size()==size){
+             auto d_key=l.back().first;
+                l.pop_back();
+                m.erase(d_key);
         }
-    }
+        
+        l.push_front({key,value});
+        m[key]=l.begin();
+        
+        
     }
 };
 
