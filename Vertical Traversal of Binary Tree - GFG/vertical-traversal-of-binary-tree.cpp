d6a3e90ac1cdb6e6ca67f2998a48db46d47c7923
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-11 17:07:19 +0530

    Added solution - LeetHub

---
+++ Vertical Traversal of Binary Tree - GFG/vertical-traversal-of-binary-tree.cpp
@@ -0,0 +1,170 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+          
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+        
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *A)
+    {
+        //Your code here
+        vector<vector<int>> ans;
+    
+    queue<pair<int,Node*>> q;
+    
+    map<int,vector<int>> mp;
+    vector<int> res;
+    q.push({0,A});
+    if(!A) return res;
+    
+    while(!q.empty()){
+        auto rib=q.front();
+        q.pop();
+        int num=rib.first;
+        Node* temp=rib.second;
+        
+        mp[num].push_back(temp->data);
+        
+        if(temp->left) q.push({num-1,temp->left});
+        if(temp->right) q.push({num+1,temp->right});
+    }
+    
+    for(auto x:mp)
+    ans.push_back(x.second);
+    
+     
+     
+     int n=ans.size();
+     
+     for(int i=0;i<n;i++){
+         for(int j=0;j<ans[i].size();j++){
+             res.push_back(ans[i][j]);
+         }
+     }
+     
+     return res;
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        // string c;
+        // getline(cin,c);
+        Solution obj;
+    	Node* root = buildTree(s);
+    	
+    	vector <int> res = obj.verticalOrder(root);
+    	for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+	return 0;
+}
+
+
+  // } Driver Code Ends
