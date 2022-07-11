9e511d7fd7e64a94f58366e64451401002d2f26e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-11 07:26:16 +0530

    Time: 3 ms (94.20%), Space: 12.8 MB (91.30%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,24 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode *&root, int ele){
-        if(root==NULL)
-            return root=new TreeNode(ele);
-        
-        if(root->val>ele)
-            helper(root->left,ele);
-        
-        else helper(root->right,ele);
-        
-        return root;
+    TreeNode *helper(TreeNode *&root, int ele){
+     if(root==NULL) 
+     return root=new TreeNode(ele);
+     
+     if(root->val>ele)
+     helper(root->left,ele);
+     
+     else helper(root->right,ele);
+     
+     return root;
+ }
+    TreeNode* bstFromPreorder(vector<int>& A) {
+       TreeNode* root=NULL;
+    
+    for(auto x:A){
+        helper(root,x);
     }
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode *root=NULL;
-        
-        for(auto x:preorder){
-            helper(root,x);
-        }
-        
-        return root;
+    
+    return root;
     }
 };
