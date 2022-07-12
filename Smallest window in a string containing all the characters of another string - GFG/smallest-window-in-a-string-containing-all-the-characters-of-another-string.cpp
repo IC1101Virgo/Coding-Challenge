35103ee7200e039ad421f792f27796a09cead8be
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-12 23:38:04 +0530

    Added solution - LeetHub

--- Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -9,45 +9,44 @@ class Solution
     public:
     //Function to find the smallest window in the string s consisting
     //of all the characters of string p.
-    string smallestWindow (string s, string p)
+    string smallestWindow (string s, string t)
     {
         // Your code here
-        int n=s.length(), ml=p.length();
+         unordered_map<char,int> tmap;
         
-        unordered_map<char,int>mpp;
+        for(int i=0;i<t.size();i++)
+            tmap[t[i]]++;
         
-        for(int i=0;i<ml;i++)
-        mpp[p[i]]++;
+        int end=0, start=0, count=t.size(), maxlen=INT_MAX, lo=0;
         
-        int count=mpp.size();
-       int j=0,i=0,start;
-       string ans="";
-       int len=INT_MAX;
-       
-       while(j<s.length()){
-           
-           if(mpp.find(s[j])!=mpp.end()){
-               mpp[s[j]]--;
-               if(mpp[s[j]]==0){
-                   count--;
-               }
-           }
-           
-           while(count==0){
-               if(len>j-i+1){
-                   len=j-i+1;
-                   ans=s.substr(i,len);
-               }
-               if(mpp.find(s[i])!=mpp.end()){
-                   mpp[s[i]]++;
-                   if(mpp[s[i]]==1)count++;
-               }
-               i++;
-           }
-           
-           j++;
-       }
-       return len!=INT_MAX ? ans : "-1" ;
+        while(end<s.size()){
+            if(tmap[s[end]]>0){
+                count--;
+            }
+            
+            tmap[s[end]]--;
+            
+            end++;
+            
+            while(count==0){
+                if(end-start<maxlen){
+                    lo=start;
+                    maxlen=end-start;
+                }
+                
+                tmap[s[start]]++;
+                
+                if(tmap[s[start]]>0)
+                count++;
+                
+                start++;
+            }
+        }
+        
+         if(maxlen!=INT_MAX){
+            return s.substr(lo,maxlen);
+        }
+        return "-1";
     }
 };
 
