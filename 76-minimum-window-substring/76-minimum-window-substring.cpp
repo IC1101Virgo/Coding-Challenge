7017dafe4367c4a425062e459fbf90459949c0a8
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-12 23:44:17 +0530

    Time: 20 ms (73.79%), Space: 16.5 MB (15.94%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        unordered_map<char,int> tmap;
+    string minWindow(string s, string p) {
+         int n=s.length(), ml=p.length();
         
-        for(int i=0;i<t.size();i++)
-            tmap[t[i]]++;
+        unordered_map<char,int>mpp;
         
-        int end=0, start=0, count=t.size(), maxlen=INT_MAX, lo=0;
+        for(int i=0;i<ml;i++)
+        mpp[p[i]]++;
         
-        while(end<s.size()){
-            if(tmap[s[end]]>0){
-                count--;
-            }
-            
-            tmap[s[end]]--;
-            
-            end++;
-            
-            while(count==0){
-                if(end-start<maxlen){
-                    lo=start;
-                    maxlen=end-start;
-                }
-                
-                tmap[s[start]]++;
-                
-                if(tmap[s[start]]>0)
-                count++;
-                
-                start++;
-            }
-        }
-        
-         if(maxlen!=INT_MAX){
-            return s.substr(lo,maxlen);
-        }
-        return "";
-        
+        int count=mpp.size();
+       int j=0,i=0,start;
+       string ans="";
+       int len=INT_MAX;
+       
+       while(j<s.length()){
+           
+           if(mpp.find(s[j])!=mpp.end()){
+               mpp[s[j]]--;
+               if(mpp[s[j]]==0){
+                   count--;
+               }
+           }
+           
+           while(count==0){
+               if(len>j-i+1){
+                   len=j-i+1;
+                   ans=s.substr(i,len);
+               }
+               if(mpp.find(s[i])!=mpp.end()){
+                   mpp[s[i]]++;
+                   if(mpp[s[i]]==1)count++;
+               }
+               i++;
+           }
+           
+           j++;
+       }
+       return len!=INT_MAX ? ans : "" ;
     }
 };
