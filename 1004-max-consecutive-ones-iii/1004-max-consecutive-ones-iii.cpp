cb4a19ffef5226031108c2eca1dcd73a91eea5d9
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-12 22:01:30 +0530

    Time: 82 ms (69.24%), Space: 55.3 MB (92.24%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,8 +3,7 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
         
-        int i=0, j=0;
-        int maxx=INT_MIN;
+        int ans=INT_MIN,i=0,j=0;
         
         while(j<n){
             if(k>=0 && nums[j++]==0)
@@ -13,9 +12,9 @@ public:
             if(k<0 && nums[i++]==0)
                 k++;
             
-            maxx=max(j-i,maxx);
+            ans=max(ans,j-i);
         }
         
-        return maxx;
+        return ans;
     }
 };
