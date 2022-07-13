f8700b80c663d3435d68e1e2b5d27737cd9c033e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-13 22:47:46 +0530

    Time: 58 ms (69.81%), Space: 23.6 MB (26.89%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<vector<bool>>at,pa;
-    
-    void dfs(vector<vector<int>>& h, vector<vector<bool>> &mat,int i, int j, vector<vector<int>> &ans){
+    vector<vector<bool>> pa,at;
+    void dfs(vector<vector<int>>& h,vector<vector<bool>>& mat,int i, int j, vector<vector<int>>& ans){
         int n=h.size(), m=h[0].size();
         
-        if(mat[i][j])
+        if( mat[i][j]==1)
             return;
+        
         mat[i][j]=1;
         
-        if(at[i][j]==1 && pa[i][j]==1)
+        if(pa[i][j]==1 && at[i][j]==1)
             ans.push_back({i,j});
         
-        if(i+1<n && h[i+1][j]>=h[i][j]) dfs(h,mat,i+1,j,ans);
-        if(i-1>=0 && h[i-1][j]>=h[i][j]) dfs(h,mat,i-1,j,ans);
-        if(j+1<m && h[i][j+1]>=h[i][j]) dfs(h,mat,i,j+1,ans);
-        if(j-1>=0 && h[i][j-1]>=h[i][j]) dfs(h,mat,i,j-1,ans);
-        
+        if(i-1>=0 && h[i-1][j]>=h[i][j])  dfs(h,mat,i-1,j,ans);
+        if(i+1<n && h[i+1][j]>=h[i][j])  dfs(h,mat,i+1,j,ans);
+        if(j-1>=0 && h[i][j-1]>=h[i][j])  dfs(h,mat,i,j-1,ans);
+        if(j+1<m && h[i][j+1]>=h[i][j])  dfs(h,mat,i,j+1,ans);
+                
+
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
         int n=h.size(), m=h[0].size();
         
+        
+        
+       at=pa=vector<vector<bool>> (n,vector<bool>(m,0));
+        vector<vector<int>> ans;
         if(!n)
             return ans;
-        
-        at=pa=vector<vector<bool>> (n,vector<bool>(m,0));
-        
         for(int i=0;i<n;i++){
             dfs(h,pa,i,0,ans);
             dfs(h,at,i,m-1,ans);
+
         }
         
         for(int i=0;i<m;i++){
             dfs(h,pa,0,i,ans);
             dfs(h,at,n-1,i,ans);
+
         }
         
+        
         return ans;
+        
     }
 };
