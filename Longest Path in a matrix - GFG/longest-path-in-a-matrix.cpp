563b448461d5381492efef3baf8e181c8e260a80
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-14 09:37:44 +0530

    Added solution - LeetHub

--- Longest Path in a matrix - GFG/longest-path-in-a-matrix.cpp
+++ Longest Path in a matrix - GFG/longest-path-in-a-matrix.cpp
@@ -5,36 +5,39 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
-      int dx[4] = {-1,1,0,0};
-     int dy[4] = {0,0,-1,1};
-    int path(vector<vector<int>>& matrix , int n , int m , int x ,int y ,vector<vector<int>>&t){
-        if(x < 0 || y <0 || x>=n || y>=m){
-            return 0 ;
-        }
-        if(t[x][y] != -1){
-            return t[x][y];
-        }
-        int val = 1 ;
-        for(int i = 0 ; i<4 ; i++){
-            int nx = x+ dx[i] ;
-            int ny = y + dy[i] ;
-            if(nx >= 0 && ny >= 0  && nx < n && ny < m && matrix[nx][ny] > matrix[x][y]){
-                val = max(val , 1 + path(matrix , n , m, nx ,ny,t)) ;
+    int dx[4]={1,-1,0,0};
+    int dy[4]={0,0,1,-1};
+    int path(vector<vector<int>>& mat, int i, int j, vector<vector<int>>& v){
+        int n=mat.size(),m=mat[0].size();
+        
+        if(i<0 || i>=n || j<0 || j>=m)
+        return 0;
+        
+        if(v[i][j]!=-1)
+        return v[i][j];
+        
+        int val=1;
+        
+        for(int k=0;k<4;k++){
+            int x=i+dx[k], y=j+dy[k];
+            if(x<n && x>=0 && y<m && y>=0 && mat[i][j]<mat[x][y]){
+                val=max(val,1+path(mat,x,y,v));
             }
         }
-        return t[x][y] = val ;
         
+        return v[i][j]=val;
     }
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
+    int longestIncreasingPath(vector<vector<int>>& mat) {
         // Code here
-          int maxv=0;
-       // int n=mat.size(), m=mat[0].size();
-        int n= matrix.size()  , m= matrix[0].size();
-        vector<vector<int>>t(n+1,vector<int>(m+1,-1)) ;
+        int n=mat.size(), m=mat[0].size();
+        
+        int maxv=0;
+        
+        vector<vector<int>> v(n,vector<int>(m,-1));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                maxv=max(maxv,path(matrix,n,m,i,j,t));
+                maxv=max(maxv,path(mat,i,j,v));
             }
         }
         
