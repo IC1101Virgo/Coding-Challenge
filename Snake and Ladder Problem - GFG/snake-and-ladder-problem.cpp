d6b334b8b3c5589b97cdc4a7e4e578e91bb493bc
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-14 13:06:01 +0530

    Added solution - LeetHub

--- Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
+++ Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
@@ -11,38 +11,43 @@ class Solution{
 public:
     int minThrow(int n, int arr[]){
         // code here
-       unordered_map<int,int>m;
-    
-    for(int i=0;i<2*n;i+=2){
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<2*n;i+=2){
             m[arr[i]]=arr[i+1];
         }
-
-   
-    queue<int>q;
-    q.push(1);
-    vector<int>vis(101,0);
-    vis[1]=1;
-    int cnt=0;
-    while(!q.empty())
-    {   
-       
-        int size=q.size();
-        while(size--)
-        { 
-        int x=q.front(); q.pop();
-        if(x==30) return cnt;
-        for(int i=0;i<6;i++)
-        {    
-            int z=x+1+i;
-            if(m[z]) z=m[z];
-           
-           
-             if(vis[z]==0 ){ q.push(z); vis[z]=1;}
+        
+        vector<int> vis(31,0);
+        queue<int> q;
+        
+        q.push(1);
+        vis[1]=1;
+        int cnt=0;
+        
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int temp=q.front();
+                q.pop();
+                
+                if(temp==30)
+                return cnt;
+                
+                for(int i=1;i<=6;i++){
+                    int z=temp+i;
+                    
+                    if(m.find(z)!=m.end())
+                    z=m[z];
+                    
+                    if(vis[z]==0)
+                    q.push(z);
+                }
+            }
+            
+            cnt++;
         }
-        }
-       cnt++;
-    }
-    return -1;
+        
+        return -1;
     }
 };
 
