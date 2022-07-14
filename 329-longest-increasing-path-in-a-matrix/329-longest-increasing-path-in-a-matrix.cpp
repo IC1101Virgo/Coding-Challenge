af82935eb6a8a9f36151e0cd46a4f0c9450d499b
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-14 09:51:31 +0530

    Time: 47 ms (92.40%), Space: 15.8 MB (78.95%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    int dp[201][201];
-    
-    int dfs(vector<vector<int>>& mat,int i,int j, int pre){
-        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size() || pre>=mat[i][j])
+    int dx[4]={1,-1,0,0};
+    int dy[4]={0,0,1,-1};
+    int dfs(vector<vector<int>>& mat, int n, int m, int i, int j, vector<vector<int>>& dp){
+        if(i<0||i>=n||j<0||j>=m)
             return 0;
         
-        if(dp[i][j]) return dp[i][j];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
-        int l=dfs(mat,i-1,j,mat[i][j]);
-        int r=dfs(mat,i,j-1,mat[i][j]);
-        int u=dfs(mat,i+1,j,mat[i][j]);
-        int d=dfs(mat,i,j+1,mat[i][j]);
+        int val=1;
         
-        return dp[i][j]=max({l,r,u,d})+1;
+        for(int k=0;k<4;k++){
+            int x=i+dx[k], y=j+dy[k];
+            
+            if(x>=0 && x<n && y>=0 && y<m && mat[i][j]<mat[x][y])
+                val=max(val,1+dfs(mat,n,m,x,y,dp));
+        }
+        
+        return dp[i][j]=val;
+        
     }
     int longestIncreasingPath(vector<vector<int>>& mat) {
-        int maxv=0;
         int n=mat.size(), m=mat[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        int maxv=0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                maxv=max(maxv,dfs(mat,i,j,-1));
+                maxv=max(maxv,dfs(mat,n,m,i,j,dp));
             }
         }
         
