b50937ea7ee0f8ee440ebac71d8797d87bca862b
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-14 07:56:53 +0530

    Time: 830 ms (30.07%), Space: 7.9 MB (73.84%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
-		//check if all the alphabets in the word is checked
-        if(n == word.size()) return true; 
+    bool dfs(vector<vector<char>> &A, string B, int i, int j, int idx){
+    int n=A.size(), m=A[0].size();
+    
+    if(idx==B.size())
+    return 1;
+    
+    if(i<0 || i>=n || j<0 || j>=m || A[i][j]!=B[idx])
+    return 0;
+    
+        A[i][j]='1';
         
-		//check if i and j are out of bound or if the characters aren't equal
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
+    bool check=dfs(A,B,i+1,j,idx+1)||
+                dfs(A,B,i-1,j,idx+1)||
+                dfs(A,B,i,j+1,idx+1)||
+                dfs(A,B,i,j-1,idx+1);
         
-		//mark as visited 
-        board[i][j] = '0';
-        
-		//branch out in all 4 directions
-        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
-                        DFS(board, word, i, j + 1, n + 1) ||  //right
-                        DFS(board, word, i - 1, j, n + 1) ||  //up
-                        DFS(board, word, i, j - 1, n + 1);  //left
-        
-		//change the character back for other searches
-        board[i][j] = word[n];
-		
-        return status;
-    }
-    bool exist(vector<vector<char>>& b, string w) {
-        int n=b.size(),m=b[0].size();
-         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(w[0]==b[i][j] && DFS(b,w,i,j,0))
-                    return true;
-            }
+        A[i][j]=B[idx];
+                
+    return check;
+                
+}
+    bool exist(vector<vector<char>>& A, string B) {
+        int n=A.size(), m=A[0].size();
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(B[0]==A[i][j] && dfs(A,B,i,j,0))
+            return 1;
         }
-        
-        return false;
+    }
+    
+    return 0;
     }
 };
