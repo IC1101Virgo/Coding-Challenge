8e5d997aa8484e912458b329e701587bd76c5893
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-14 07:33:07 +0530

    Time: 39 ms (10.01%), Space: 10 MB (77.08%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& b, int i,int j){
-        int n=b.size(), m=b[0].size();
+    void dfs(vector<vector<char>>& board, int i, int j){
+        int n=board.size(), m=board[0].size();
         
-        if(i==n || j==m ||i<0 ||j<0|| b[i][j]!='O')
+        if(i<0 ||i>=n || j<0 || j>=m || board[i][j]!='O')
             return;
         
-        b[i][j]='a';
+        board[i][j]='a';
         
-        dfs(b,i+1,j);
-        dfs(b,i-1,j);
-        dfs(b,i,j+1);
-        dfs(b,i,j-1);
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
     }
-    void solve(vector<vector<char>>& b) {
-        int n=b.size(), m=b[0].size();
+    void solve(vector<vector<char>>& board) {
+        int n=board.size(), m=board[0].size();
         
         for(int i=0;i<n;i++){
-            if(b[i][0]=='O')
-                dfs(b,i,0);
+            if(board[i][0]=='O')
+                dfs(board,i,0);
             
-            if(b[i][m-1]=='O')
-                dfs(b,i,m-1);
+            if(board[i][m-1]=='O')
+                dfs(board,i,m-1);
         }
         
         for(int i=0;i<m;i++){
-            if(b[0][i]=='O')
-                dfs(b,0,i);
+            if(board[0][i]=='O')
+                dfs(board,0,i);
             
-            if(b[n-1][i]=='O')
-                dfs(b,n-1,i);
+            if(board[n-1][i]=='O')
+                dfs(board,n-1,i);
         }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(b[i][j]=='O')
-                    b[i][j]='X';
+                if(board[i][j]=='O')
+                    board[i][j]='X';
                 
-                if(b[i][j]=='a')
-                    b[i][j]='O';
+                if(board[i][j]=='a')
+                    board[i][j]='O';
             }
         }
+        
+        return;
     }
 };
