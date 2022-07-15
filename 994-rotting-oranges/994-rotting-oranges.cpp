3de632ce08e38865b1b69561d4c568afb3739250
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-15 11:40:57 +0530

    Time: 13 ms (44.01%), Space: 13.1 MB (47.28%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -3,47 +3,50 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
         
-        queue<pair<int,int>>q;
-        int orange=0;
+      
+        int org=0;
+        queue<pair<int,int>> q;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                if(grid[i][j]==1)
+                    org++;
+                
                 if(grid[i][j]==2)
                     q.push({i,j});
-                
-                if(grid[i][j]==1)
-                    orange++;
             }
         }
         
+        int time=0;
         int dx[4]={1,-1,0,0};
         int dy[4]={0,0,1,-1};
-        int time=0;
         
-        while(!q.empty() && orange!=0){
-            int s=q.size();
+        while(!q.empty() && org!=0){
+            int size=q.size();
             
-            for(int i=0;i<s;i++){
-            auto temp=q.front();
-            q.pop();
-            
-            
-            for(int i=0;i<4;i++){
-              int x=temp.first+dx[i];
-               int y=temp.second+dy[i];
+            while(size--){
+                auto temp=q.front();
                 
-                if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1){
-                    grid[x][y]=2;
-                    q.push({x,y});
-                    orange--;
+                q.pop();
+                
+                for(int k=0;k<4;k++){
+                    int x=temp.first+dx[k];
+                    int y=temp.second+dy[k];
+                    
+                    if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1){
+                        grid[x][y]=2;
+                        org--;
+                        q.push({x,y});
+                        }
                 }
             }
-            }   
+            
             time++;
         }
         
-        if(orange==0 )
+        if(org==0)
             return time;
         
-        return -1;
+        else return -1;
     }
 };
