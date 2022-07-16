16a9632d7bf5e68a3a4f7eba794d815a2a37fdad
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-16 06:36:10 +0530

    Time: 87 ms (19.65%), Space: 9.7 MB (37.21%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.cpp
+++ 147-insertion-sort-list/147-insertion-sort-list.cpp
@@ -10,23 +10,24 @@
  */
 class Solution {
 public:
-    ListNode* insertionSortList(ListNode* A) {
-        ListNode *p=A->next;
-    
-    if(p==NULL)
-    return A;
-    
-    while(p!=NULL){
-        ListNode *p2=A;
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *ptr=head->next;
         
-        while(p!=p2){
-            if(p2->val>p->val)
-            swap(p->val,p2->val);
-            p2=p2->next;
+        if(ptr==NULL)
+            return head;
+        
+        while(ptr){
+            ListNode *p=head;
+            
+            while(p!=ptr){
+                if(p->val>ptr->val)
+                    swap(p->val,ptr->val);
+                p=p->next;
+            }
+            
+            ptr=ptr->next;
         }
-        p=p->next;
-    }
-    
-    return A;
+        
+        return head;
     }
 };
