9de1ff7b3cabe4c46574a9b70295e1ea64a49e6c
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-17 08:36:34 +0530

    Time: 279 ms (50.60%), Space: 81.5 MB (45.31%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -9,30 +9,32 @@ public:
         for(int i=0;i<n;i++){
             while(!left.empty() && h[left.top()]>=h[i])
                 left.pop();
-                if(!left.empty())
-                    l[i]=left.top();
-                else l[i]=-1;
-                left.push(i);
             
+            if(left.empty())
+                l[i]=-1;
+            else l[i]=left.top();
+            
+            left.push(i);
         }
         
         for(int i=n-1;i>=0;i--){
             while(!right.empty() && h[right.top()]>=h[i])
                 right.pop();
-                if(!right.empty())
-                    r[i]=right.top();
-                else r[i]=n;
-                right.push(i);
             
+            if(right.empty())
+                r[i]=n;
+            else r[i]=right.top();
+            
+            right.push(i);
         }
         
-        int ans=INT_MIN, maxans=INT_MIN;
+        int ans=INT_MIN, maxa=INT_MIN;
         
         for(int i=0;i<n;i++){
-          ans=h[i]*(r[i]-l[i]-1);
-            maxans=max(ans,maxans);
+            ans=h[i]*(r[i]-l[i]-1);
+            maxa=max(maxa,ans);
         }
         
-        return maxans;
+        return maxa;
     }
 };
