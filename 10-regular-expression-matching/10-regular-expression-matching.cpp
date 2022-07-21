9a94834334a47594831d92dbe6e908726fec7ae7
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 12:11:14 +0530

    Time: 12 ms (46.71%), Space: 7 MB (62.96%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -4,7 +4,6 @@ public:
         int n=s.size(), m=p.size();
         
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
-        
         dp[0][0]=1;
         
         for(int i=1;i<=m;i++){
@@ -12,11 +11,14 @@ public:
                 dp[0][i]=dp[0][i-2];
             
             else dp[0][i]=0;
+            
+            
         }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(p[j-1]=='*'){
+                if(p[j-1]=='*')
+                {
                     dp[i][j]=dp[i][j-2];
                     
                     if(s[i-1]==p[j-2]||p[j-2]=='.')
