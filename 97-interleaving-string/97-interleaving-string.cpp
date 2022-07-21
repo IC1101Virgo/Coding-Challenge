e287efae62fde60462fc24fc99177a92c43643df
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 11:48:42 +0530

    Time: 3 ms (82.59%), Space: 7.2 MB (43.59%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,13 +3,13 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n1=s1.size(), n2=s2.size(), n3=s3.size();
         
-        vector<vector<bool>> dp(n1+1,vector<bool>(n2+1,0));
+        if(n1+n2!=n3)
+            return 0;
         
         if(n3==0)
-            return true;
+            return 1;
         
-        if( n1+n2!=n3)
-            return false;
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
         
         for(int i=0;i<=n1;i++){
             for(int j=0;j<=n2;j++){
@@ -29,11 +29,13 @@ public:
                 else if(s1[i-1]!=s3[i+j-1] && s2[j-1]==s3[i+j-1])
                     dp[i][j]=dp[i][j-1];
                 
-                 else if(s1[i-1]==s3[i+j-1] && s2[j-1]!=s3[i+j-1])
+                else if(s2[j-1]!=s3[i+j-1] && s1[i-1]==s3[i+j-1])
                     dp[i][j]=dp[i-1][j];
                 
-                 else if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
-                    dp[i][j]=dp[i][j-1]||dp[i-1][j];
+                else if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1])
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                
+                //else dp[i][j]=0;
             }
         }
         
