ba7fd93e6d9c93bf3e98866d5a3cbfe2866bb1c9
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 10:21:27 +0530

    Time: 1910 ms (5.01%), Space: 522.9 MB (5.01%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -16,7 +16,7 @@ public:
                 if(s[i-1]==t[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                     
-                    if(dp[i][j]>ans.size()){
+                    if(dp[i][j]>val){
                         string x=s.substr(i-dp[i][j],dp[i][j]);
                         string xx=x;
                         reverse(xx.begin(),xx.end());
