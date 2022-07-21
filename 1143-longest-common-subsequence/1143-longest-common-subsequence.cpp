818501ab0b4f91f70d0d3a4c8ff28b3db464304e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 10:04:41 +0530

    Time: 37 ms (46.61%), Space: 13.1 MB (21.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,12 +4,9 @@ public:
         int n=t1.size(), m=t2.size();
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-       
-        
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                
                 if(t1[i-1]==t2[j-1])
                     dp[i][j]=1+dp[i-1][j-1];
                 
