c4b706cbb6e191584b4c0d34f53af1966537f831
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 13:50:04 +0530

    Time: 31 ms (33.67%), Space: 13.3 MB (41.56%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n=s.size();
-        
-        set<string>di;
-        
-        for(auto x:wordDict){
-            di.insert(x);
-        }
-        
-        vector<bool> dp(n+1,0);
-        
-        dp[0]=1;
-        
-       for(int i=1;i<=n;i++){
+    bool wordBreak(string A, vector<string>& B) {
+       int n=A.size();
+    
+    set<string>di;
+    
+    for(auto x:B){
+        di.insert(x);
+    }
+    
+    vector<bool> dp(n+1,0);
+    dp[0]=1;
+    
+     for(int i=1;i<=n;i++){
             for(int j=0;j<i;j++){
                 
                 if(dp[j]==1){
-                    string word=s.substr(j,i-j);
+                    string word=A.substr(j,i-j);
                         
                         if(di.find(word)!=di.end()){
                             dp[i]=1;
@@ -26,7 +25,7 @@ public:
                 }
             }
         }
-        
-        return dp[n];
+    
+    return dp[n];
     }
 };
