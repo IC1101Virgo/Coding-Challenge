6d0e324d1cb0894287b3abfb98931be27926954e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 11:14:31 +0530

    Time: 20 ms (54.99%), Space: 9 MB (56.92%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -7,21 +7,24 @@ public:
         
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                dp[0][j]=j;
                 dp[i][0]=i;
+                
+                dp[0][j]=j;
             }
         }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                
                 if(w1[i-1]==w2[j-1])
                     dp[i][j]=dp[i-1][j-1];
                 
-                else dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                else
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
             }
         }
         
         return dp[n][m];
+        
+        
     }
 };
