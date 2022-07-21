18f4abcb89e1daae3bda46f6c2fc54f284bbab77
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-21 11:28:36 +0530

    Time: 67 ms (39.10%), Space: 18.6 MB (47.60%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,7 +3,7 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size(), m=t.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>> dp(n+1,vector(m+1,0));
         
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
@@ -13,8 +13,10 @@ public:
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1])
+                if(s[i-1]==t[j-1]){
                     dp[i][j]=(dp[i-1][j]+dp[i-1][j-1])%1000000007;
+                
+                }
                 
                 else dp[i][j]=dp[i-1][j];
             }
