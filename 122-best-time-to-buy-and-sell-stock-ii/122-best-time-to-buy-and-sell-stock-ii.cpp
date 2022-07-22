126a524c25090fa297cb7eb180e7d5bd7b09ff18
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-22 11:12:38 +0530

    Time: 14 ms (36.76%), Space: 13.2 MB (21.67%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,9 +5,12 @@ public:
         
         vector<int> dp(n,0);
         
+        dp[0]=0;
+        
         for(int i=1;i<n;i++){
-            if(p[i]>p[i-1])
+            if(p[i-1]<p[i]){
                 dp[i]=dp[i-1]+p[i]-p[i-1];
+            }
             
             else dp[i]=dp[i-1];
         }
