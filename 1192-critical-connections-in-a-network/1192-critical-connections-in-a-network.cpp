c66e9ebb1d56462d211b23a28358c32f93f66489
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-23 12:35:17 +0530

    Time: 1465 ms (6.47%), Space: 188.9 MB (33.05%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    
-    void dfs(int node, int parent, vector<int> &tin, vector<int> &low, vector<int> &vis, int &timer, vector<int> adj[]){
-        vis[node]=1;
+    void dfs(int i, int parent, vector<int> &tin, vector<int> &low,vector<int> &vis, vector<int> adj[], int &timer){
+        vis[i]=1;
         
-        tin[node]=low[node]=timer++;
+        tin[i]=low[i]=timer++;
         
-        for(auto x:adj[node]){
+        for(auto x:adj[i]){
             if(x==parent)
                 continue;
             
             if(!vis[x]){
-                dfs(x,node,tin,low,vis,timer,adj);
-                low[node]=min(low[node],low[x]);
+                dfs(x,i,tin,low,vis,adj,timer);
+                low[i]=min(low[i],low[x]);
                 
-                if(low[x]>tin[node])
-                    ans.push_back({node,x});
+                if(low[x]>tin[i])
+                    ans.push_back({i,x});
             }
             
-            else low[node]=min(low[node],tin[x]);
+            else low[i]=min(low[i],tin[x]);
         }
-        
     }
-    
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<int> adj[n];
-        for(auto x:connections){
+        
+        for(auto x:connections)
+        {
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
         }
         
-        vector<int>tin(n,-1);
-        vector<int>low(n,-1);
+        vector<int> tin(n,-1);
+        vector<int> low(n,-1);
         vector<int> vis(n,0);
-        
         int timer=0;
         
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(i,-1,tin,low,vis,timer,adj);
+                dfs(i,-1,tin,low,vis,adj,timer);
             }
         }
         
