416a4ac39f75e4d46be88921df637f08249e8fae
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-23 08:56:33 +0530

    Time: 712 ms (83.22%), Space: 189.2 MB (32.38%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -2,43 +2,46 @@ class Solution {
 public:
     vector<vector<int>> ans;
     
-    void dfs(int node, int parent, vector<int> &vis, vector<int> &tin, vector<int> &low, int &timer, vector<int> adj[]) {
-   vis[node]=1;
+    void dfs(int node, int parent, vector<int> &tin, vector<int> &low, vector<int> &vis, int &timer, vector<int> adj[]){
+        vis[node]=1;
+        
         tin[node]=low[node]=timer++;
         
-        for(auto it:adj[node]){
-            if(it==parent) continue;
+        for(auto x:adj[node]){
+            if(x==parent)
+                continue;
             
-            if(!vis[it]){
-            dfs(it,node,vis,tin,low,timer,adj);
-            low[node]=min(low[node],low[it]);
-            
-            if(low[it]>tin[node])
-                ans.push_back({node,it});
+            if(!vis[x]){
+                dfs(x,node,tin,low,vis,timer,adj);
+                low[node]=min(low[node],low[x]);
+                
+                if(low[x]>tin[node])
+                    ans.push_back({node,x});
             }
             
-            else
-                low[node]=min(low[node],tin[it]);
-            
+            else low[node]=min(low[node],tin[x]);
         }
-}
-
+        
+    }
+    
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int> adj[n]; 
-	for(auto x:connections) {
-	    adj[x[0]].push_back(x[1]);
-	    adj[x[1]].push_back(x[0]); 
-	}
-	
-	vector<int> tin(n, -1);
-	vector<int> low(n, -1); 
-	vector<int> vis(n, 0); 
-	int timer = 0; 
-	for(int i = 0;i<n;i++) {
-	    if(!vis[i]) {
-	        dfs(i, -1, vis, tin, low, timer, adj); 
-	    }
-	}
+        vector<int> adj[n];
+        for(auto x:connections){
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+        
+        vector<int>tin(n,-1);
+        vector<int>low(n,-1);
+        vector<int> vis(n,0);
+        
+        int timer=0;
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,-1,tin,low,vis,timer,adj);
+            }
+        }
         
         return ans;
     }
