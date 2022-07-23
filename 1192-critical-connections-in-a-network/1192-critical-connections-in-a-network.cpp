39618a18761dd0296c0a60c6798aa3d186f6d6de
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-23 08:49:16 +0530

    Time: 1360 ms (10.52%), Space: 189.2 MB (32.38%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -3,21 +3,24 @@ public:
     vector<vector<int>> ans;
     
     void dfs(int node, int parent, vector<int> &vis, vector<int> &tin, vector<int> &low, int &timer, vector<int> adj[]) {
-    vis[node] = 1; 
-    tin[node] = low[node] = timer++; 
-    for(auto it: adj[node]) {
-        if(it == parent) continue;
+   vis[node]=1;
+        tin[node]=low[node]=timer++;
         
-        if(!vis[it]) {
-            dfs(it, node, vis, tin, low, timer, adj); 
-            low[node] = min(low[node], low[it]); 
-            if(low[it] > tin[node]) {
-               ans.push_back({node,it});
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            
+            if(!vis[it]){
+            dfs(it,node,vis,tin,low,timer,adj);
+            low[node]=min(low[node],low[it]);
+            
+            if(low[it]>tin[node])
+                ans.push_back({node,it});
             }
-        } else {
-            low[node] = min(low[node], tin[it]); 
+            
+            else
+                low[node]=min(low[node],tin[it]);
+            
         }
-    }
 }
 
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
