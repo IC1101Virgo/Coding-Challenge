7c2f6adf5d57c92466129b17c02d6c12ad1a0baa
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-24 12:13:50 +0530

    Time: 15 ms (15.30%), Space: 17.2 MB (10.87%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -7,12 +7,14 @@ public:
         if(o==n && c==n){
             res.push_back(temp);
           //  temp.clear();
-            return;
+          //  return;
         }
         
          helper(n,o+1,c,temp+'(',res);
          helper(n,o,c+1,temp+')',res);
         temp.pop_back();
+        
+        return;
     }
     vector<string> generateParenthesis(int n) {
         vector<string> res;
