aba8b7ed541845b0c9e6b74f496243947f65e366
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-24 09:19:41 +0530

    Time: 12 ms (35.42%), Space: 9.1 MB (22.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -9,15 +9,16 @@ public:
         if(dp[i][j]!=-1)
             return dp[i][j];
         
+        else{
         int a=t[i][j]+helper(t,dp,i+1,j), b=t[i][j]+helper(t,dp,i+1,j+1);
         
         return dp[i][j]=min(a,b);
+        }
     }
     int minimumTotal(vector<vector<int>>& t) {
         int n=t.size();
         
         vector<vector<int>> dp(n,vector<int>(n,-1));
-       // dp[0][0]=t[0][0];
         
         return helper(t,dp,0,0);
     }
