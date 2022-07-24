b340a6cbc535a6211b07cc01b1b4bfaed3294282
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-24 13:04:46 +0530

    Time: 23 ms (15.22%), Space: 10.1 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -11,8 +11,8 @@ public:
             dp[i][0]=dp[i-1][0]+grid[i][0];
         }
         
-        for(int j=1;j<m;j++){
-            dp[0][j]=dp[0][j-1]+grid[0][j];
+        for(int i=1;i<m;i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
         }
         
         for(int i=1;i<n;i++){
