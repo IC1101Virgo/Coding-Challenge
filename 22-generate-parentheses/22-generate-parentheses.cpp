d1aa5ce2849befa1a57986275b4c8ada34dd159f
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-24 12:13:28 +0530

    Time: 8 ms (44.07%), Space: 15.1 MB (28.70%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(int n, int o, int c, string temp, vector<string> &res){
+        if(o>n || c>n || c>o)
+            return;
+        
+        if(o==n && c==n){
+            res.push_back(temp);
+          //  temp.clear();
+            return;
+        }
+        
+         helper(n,o+1,c,temp+'(',res);
+         helper(n,o,c+1,temp+')',res);
+        temp.pop_back();
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        helper(n,0,0,"",res);
+        
+        return res;
+    }
+};
