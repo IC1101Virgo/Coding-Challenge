bcb019cf92483f125a1c0bb00ea9ad0c5ef6d3e6
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-24 13:00:04 +0530

    Time: 0 ms (100.00%), Space: 8 MB (12.57%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,21 +3,20 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
         
-        int dp[n][m];
-        memset(dp,0,sizeof(dp));
+        vector<vector<int>> dp(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             if(grid[i][0]==1)
                 break;
             
-            dp[i][0]=1;
+                dp[i][0]=1;
         }
         
         for(int i=0;i<m;i++){
             if(grid[0][i]==1)
                 break;
             
-            dp[0][i]=1;
+                dp[0][i]=1;
         }
         
         for(int i=1;i<n;i++){
@@ -25,10 +24,10 @@ public:
                 if(grid[i][j]==1)
                     continue;
                 
-                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
         
         return dp[n-1][m-1];
-        }
+    }
 };
