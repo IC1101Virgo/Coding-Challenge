838637a93c049609afd42a18fa178f514420e9e8
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-25 10:37:24 +0530

    Time: 598 ms (65.39%), Space: 174.1 MB (36.63%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,41 +1,39 @@
 class LRUCache {
 public:
     list<pair<int,int>> l;
+    unordered_map<int,list<pair<int,int>>::iterator> m;
     
-    unordered_map<int, list<pair<int,int>>::iterator>m;
-    
-    int size=0;
-    
-    void refresh(int key, int val){
+    void refresh(int key,int val){
         l.erase(m[key]);
         l.push_front({key,val});
         m[key]=l.begin();
     }
+    
+    int size=0;
     LRUCache(int capacity) {
         size=capacity;
     }
     
     int get(int key) {
-        if(m.find(key)!=m.end()){
+        if(m.find(key)==m.end())
+            return -1;
+        
+        else{
             int val=(*m[key]).second;
             refresh(key,val);
             return val;
         }
-        
-        else return -1;
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end()){
+        if(m.find(key)!=m.end())
             refresh(key,value);
-        }
         
         else{
             l.push_front({key,value});
             m[key]=l.begin();
             
-            if(m.size()>size)
-            {
+            if(m.size()>size){
                 m.erase(l.back().first);
                 l.pop_back();
             }
