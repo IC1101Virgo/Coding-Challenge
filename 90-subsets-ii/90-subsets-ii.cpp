df13a3e3ea20770e7177b734687b2967911982b8
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-25 18:00:48 +0530

    Time: 3 ms (82.53%), Space: 7.8 MB (49.41%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void helper(vector<int> &nums, vector<int> v,vector<vector<int>> &res, int ind){
-        
-            res.push_back(v);
-         
+    void helper(vector<int>& nums, vector<vector<int>> &res, vector<int> temp, int ind){
+       
+            res.push_back(temp);
+            
         
         for(int i=ind;i<nums.size();i++){
-            if(i!=ind && nums[i]==nums[i-1]) continue;
+            if(i!=ind && nums[i]==nums[i-1])
+                continue;
             
-            v.push_back(nums[i]);
-            helper(nums,v,res,i+1);
-            v.pop_back();
+            temp.push_back(nums[i]);
+            helper(nums,res,temp,i+1);
+            temp.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> v;
         vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
         
-        helper(nums,v,res,0);
+        helper(nums,res,{},0);
+        
         return res;
     }
 };
