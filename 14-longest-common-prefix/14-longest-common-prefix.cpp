570e174047d1ec4c689e197861e69adddd1443a7
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-26 16:11:31 +0530

    Time: 7 ms (54.56%), Space: 9.9 MB (10.51%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,22 +1,64 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n=strs.size();
+    struct Trie{
+        Trie *ischild[26];
+        bool isword;
+        int cnt;
+        Trie(){
+            for(int i=0;i<26;i++)
+                ischild[i]=NULL;
+            isword=false;
+            cnt=0;
+        }
+    };
+    
+    Trie *root;
+    
+    void insert(Trie *root, string str){
+        Trie *curr=root;
         
-        string x=strs[0];
-        
-        for(int i=1;i<strs.size();i++){
-           
-            for(int j=0;j<x.size();j++){
-                if(strs[i][j]!=x[j]){
-                    x=x.substr(0,j);
-                    break;
-                }
-                
-                
+        for(auto i:str){
+            if(curr->ischild[i-'a']==NULL){
+                Trie *temp=new Trie();
+                curr->ischild[i-'a']=temp;
             }
+            
+            curr=curr->ischild[i-'a'];
+            curr->cnt++;
         }
         
-        return x;
+        curr->isword=true;
+    }
+    
+    void search(Trie* root, string str, string &res, int len){
+        
+        Trie *curr=root;
+        
+        for(auto i:str){
+            if(curr->ischild[i-'a']==NULL)
+                break;
+            
+            if(curr->ischild[i-'a']->cnt== len)
+                res+=i;
+            
+            else break;
+            
+            curr =curr->ischild[i-'a'];
+            
+        }
+    }
+    
+    string longestCommonPrefix(vector<string>& strs) {
+        Trie *root=new Trie();
+        
+        for(int i=0;i<strs.size();i++){
+            insert(root,strs[i]);
+        }
+        
+        string res="";
+        
+        search(root,strs[0],res, strs.size());
+        
+        return res;
     }
 };
