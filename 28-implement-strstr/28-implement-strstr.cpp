e0943ce648fd6b860801769efa19d1f306985356
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-26 14:37:43 +0530

    Time: 4 ms (31.44%), Space: 6.2 MB (90.45%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,62 +1,62 @@
 class Solution {
 public:
-    void makelps(string nee, int m, int lps[]){
-        int i=1, len=0;
+    void makelps(const string nee, int m, int lps[]){
+    int len=0, i=1;
+    lps[0]=0;
+    
+    while(i<m){
+        if(nee[len]==nee[i]){
+            len++;
+            lps[i]=len;
+            i++;
+        }
         
-        lps[0]=0;
-        
-        while(i<m){
-            if(nee[i]==nee[len]){
-                len++;
-                lps[i]=len;
-                i++;
+        else{
+            if(len!=0){
+                len=lps[len-1];
             }
             
-            else{
-                if(len!=0) len=lps[len-1];
-                
-                else{
-                    lps[i]=0;
-                    i++;
-                }
+            else 
+            {   lps[i]=0;
+                i++;
             }
         }
     }
+    
+    return;
+}
     int strStr(string hay, string nee) {
         int n=hay.size(), m=nee.size();
-        
-        int lps[m];
-        
-        makelps(nee,m,lps);
-        
-        if(m==0)
-            return 0;
-        
-        int i=0, j=0;
-        int c=0;
-        
-        while(i<n){
-            
-            if(hay[i]==nee[j]){
-                i++;
-                j++;
-            }
-            
-            if(j==m){
-                c=1;
-                break;
-                j=lps[j-1];
-            }
-            
-            if(i<n && hay[i]!=nee[j]){
-                if(j!=0) j=lps[j-1];
-                
-                else i++;
-            }
-            
-      
+    
+    int lps[m];
+    
+    makelps(nee,m,lps);
+    
+    int i=0, j=0;
+    int c=0;
+    
+    while(i<n){
+        if(hay[i]==nee[j]){
+            i++;
+            j++;
         }
         
-        return c==1?i-j:-1;
+        if(j==m){
+            c=1;
+            break;
+           // j=lps[j-1];
+        }
+        
+        if(i<n && hay[i]!=nee[j]){
+            if(j!=0)
+            j=lps[j-1];
+            
+            else{
+                i++;
+            }
+        }
+    }
+    
+    return c==1?i-j:-1;
     }
 };
