f46ec69abf91abf0399934869011522b5330f864
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-26 10:50:57 +0530

    Time: 7 ms (54.56%), Space: 9.2 MB (81.22%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -6,8 +6,7 @@ public:
         string x=strs[0];
         
         for(int i=1;i<strs.size();i++){
-            int len=0;
-            string temp="";
+           
             for(int j=0;j<x.size();j++){
                 if(strs[i][j]!=x[j]){
                     x=x.substr(0,j);
