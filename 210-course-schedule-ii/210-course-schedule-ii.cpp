30e8bbe84af8ce0a58dc3525c1bba2f2c3ca7d8e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-27 17:05:43 +0530

    Time: 35 ms (48.05%), Space: 14.6 MB (29.82%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool iscycle(vector<int> &vis, vector<int> &dfsvis, int i, vector<int> adj[]){
+    bool iscycle(vector<int> &vis, vector<int> &dfsvis, int i, vector<int> adj[],stack<int> &s){
         vis[i]=1;
         dfsvis[i]=1;
         
         for(auto edge:adj[i]){
             if(!vis[edge]){
-                if(iscycle(vis,dfsvis,edge,adj))
+                if(iscycle(vis,dfsvis,edge,adj,s))
                     return true;
             }
             else if(dfsvis[edge]==true)
@@ -14,22 +14,23 @@ public:
         }
         
         dfsvis[i]=0;
+        s.push(i);
         
         return false;
     }
     
-    void topo(vector<int> &vis, vector<int> adj[], int i,stack<int> &s){
-        vis[i]=1;
+//     void topo(vector<int> &vis, vector<int> adj[], int i,stack<int> &s){
+//         vis[i]=1;
         
-        for(auto edge:adj[i]){
-            if(!vis[edge]){
-                topo(vis,adj,edge,s);
-            }
-        }
+//         for(auto edge:adj[i]){
+//             if(!vis[edge]){
+//                 topo(vis,adj,edge,s);
+//             }
+//         }
         
-        s.push(i);
-        return;
-    }
+//         s.push(i);
+//         return;
+//     }
     
     vector<int> findOrder(int num, vector<vector<int>>& pre) {
         vector<int> adj [num+1];
@@ -39,13 +40,13 @@ public:
         }
         
         vector<int> res;
-        
+        stack<int> s;
         vector<int> vis(num,0);
         vector<int> dfsvis(num,0);
         
         for(int i=0;i<num;i++){
             if(!vis[i]){
-                if(iscycle(vis,dfsvis,i,adj)){
+                if(iscycle(vis,dfsvis,i,adj,s)){
                     //cout<<i;
                     return res;
 
@@ -53,15 +54,15 @@ public:
             }
         }
       //  cout<<"bahr";
-        stack<int> s;
         
-        for(auto &i: vis)i =0;
         
-        for(int i=0;i<num;i++){
-            if(!vis[i]){
-                topo(vis,adj,i,s);
-            }
-        }
+//         for(auto &i: vis)i =0;
+        
+//         for(int i=0;i<num;i++){
+//             if(!vis[i]){
+//                 topo(vis,adj,i,s);
+//             }
+//         }
         
         while(!s.empty()){
             res.push_back(s.top());
