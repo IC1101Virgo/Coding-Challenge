d38f74d7397a28db83257801133e1049f8b2bba0
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-27 17:59:33 +0530

    Time: 734 ms (91.90%), Space: 161 MB (58.82%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-         int n=matrix.size();
-        int m=matrix[0].size();
-        int dp[n+1][m+1];//2-ddp for prefix sum
-       
-       
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0||j==0)
-                {
-                    dp[i][j]=0;
+    int subArray(vector<int>&a, int k){
+        unordered_map<int,int>m;
+        int ans = 0;
+        int s = 0;
+        for(auto i: a){
+            s += i;
+            if(s == k)ans++;
+            if(m.find(s-k) != m.end())ans += m[s-k];
+            m[s]++;
+        }
+        return ans;
+    }
+    
+    int numSubmatrixSumTarget(vector<vector<int>>& mat, int t) {
+        int n=mat.size(), m=mat[0].size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            vector<int>arr(m,0);
+            for(int j=i;j>=0;j--){
+                for(int k=0; k<m; k++){
+                    arr[k] += mat[j][k];
                 }
-                else{
-                    
-                dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+matrix[i-1][j-1];//using prefix dp formulae
-                    
-                }
+                ans += subArray(arr, t);
             }
         }
-        int count=0;
-		//generate all possible sub matrix.
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                for(int k=i;k<=n;k++)
-                {
-                    for(int l=j;l<=m;l++)
-                    {
-                      int ans=dp[i-1][j-1]+dp[k][l]-dp[i-1][l]-dp[k][j-1];
-                        if(ans==target)
-                        {
-                            count++;
-                        }
-                    }
-                    
-                }
-                
-            }
-        }
-        return count;
-       
+        
+        return ans;
     }
 };
