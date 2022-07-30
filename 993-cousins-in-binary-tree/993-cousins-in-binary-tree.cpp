ba16adf9632b35a74cda33469274d93d29b39b75
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-30 08:18:42 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (51.25%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -11,45 +11,43 @@
  */
 class Solution {
 public:
-    bool isCousins(TreeNode* A, int x, int y) {
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(!root)
+            return 0;
         
-    
-    queue<TreeNode *> q;
-    
-    q.push(A);
-   
-    
-    while(!q.empty() ){
-        int n=q.size();
-        bool f1=0, f2=0;
         
-        while(n--){
-            TreeNode *temp=q.front();
-            q.pop();
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size=q.size();
+            bool f1=0,f2=0;
             
-            if(temp->val==x)
-                f1=1;
-            if(temp->val==y)
-                f2=1;
-            
-            if(temp->left && temp->right){
-                if(temp->left->val==x && temp->right->val==y || temp->right->val==x && temp->left->val==y)
-                    return false;
-            }
-            
-            
+            while(size--){
+                TreeNode *temp=q.front();
+                q.pop();
+                
+                if(temp->val==x)
+                    f1=1;
+                
+                if(temp->val==y)
+                    f2=1;
+                
+                if(temp->left && temp->right)
+                {
+                    if((temp->left->val == x && temp->right->val == y) || (temp->right->val == x && temp->left->val == y))
+                        return 0;
+                        //break;
+                }
+                
                 if(temp->left) q.push(temp->left);
                 if(temp->right) q.push(temp->right);
+            }
             
-            
+            if(f1 && f2)
+                return 1;
         }
         
-       
-             if(f1 && f2)
-            return 1;
-    }
-    
-    return 0;
-    
+        return 0;
     }
 };
