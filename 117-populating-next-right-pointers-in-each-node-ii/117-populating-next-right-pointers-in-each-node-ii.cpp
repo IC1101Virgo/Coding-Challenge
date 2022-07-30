789ccb89945c27dac2726cee034edb8d568af20a
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-30 12:58:26 +0530

    Time: 19 ms (67.76%), Space: 17.5 MB (64.22%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -19,8 +19,8 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-       if(!root)
-            return NULL;
+        if(!root)
+            return root;
         
         queue<Node*> q;
         
@@ -28,20 +28,24 @@ public:
         
         while(!q.empty()){
             int n=q.size();
-            Node* prev=NULL;
             
             for(int i=0;i<n;i++){
-                Node* curr=q.front();
-                
-                if(i>0)
-                    prev->next=curr;
-                
+                Node *temp=q.front();
                 q.pop();
                 
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
+                if(i==n-1){
+                    temp->next=NULL;
+                }
                 
-                prev=curr;
+                else{
+                    temp->next=q.front();
+                }
+                
+                if(temp->left)
+                    q.push(temp->left);
+                
+                if(temp->right)
+                    q.push(temp->right);
             }
         }
         
