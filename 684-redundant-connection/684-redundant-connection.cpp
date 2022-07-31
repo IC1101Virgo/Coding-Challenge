718fb3abaa975d8b7fc2ab575b2ad97d164137c4
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-07-31 20:23:22 +0530

    Time: 11 ms (65.87%), Space: 8.6 MB (99.35%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int findpar(int node, vector<int> &par){
+        if(node==par[node])
+            return node;
+        
+        else 
+            return par[node]=findpar(par[node],par);
+        
+    }
+    
+    void uni(vector<int> &par, vector<int> &rank, int a, int b){
+        a=findpar(a,par);
+        b=findpar(b,par);
+        
+       if(a==b)
+           return;
+        
+        else{
+            par[a]=b;
+            return;
+        }
+        
+        
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> ans(2,0);
+        int n=edges.size();
+        
+        vector<int>par (n+1,0);
+        
+        for(int i=1;i<=n;i++){
+            par[i]=i;
+        }
+        
+        vector<int> rank(n+1,0);
+        
+        for(int i=0;i<n;i++){
+            
+            if(findpar(edges[i][0],par)==findpar(edges[i][1],par))
+            {
+                ans[0]=edges[i][0];
+                ans[1]=edges[i][1];
+                break;
+                
+            }
+            
+            else
+            uni(par,rank,edges[i][0],edges[i][1]);
+        }
+        
+        return ans;
+        
+    }
+};
