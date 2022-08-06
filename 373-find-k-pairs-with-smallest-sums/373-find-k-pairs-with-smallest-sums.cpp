883055a5696f6364233e3c0631a67a784b33e23e
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-06 12:24:54 +0530

    Time: 410 ms (74.03%), Space: 119.4 MB (34.04%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> pip;
-    
+    typedef pair<int, pair<int,int>> pip;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
         
         int n1=nums1.size(), n2=nums2.size();
-        priority_queue<pip,vector<pip>,greater<pip>> pq;
         
-        for(int i=0;i<n1;i++){
-            pq.push({nums1[i]+nums2[0],{i,0}});
+        priority_queue<pip, vector<pip>, greater<pip>> pq;
+        
+        for(int i=0; i<n2; i++){
+            pq.push({nums1[0]+nums2[i], {0, i}});
         }
         
         while(ans.size()<k && !pq.empty()){
-            pip imp=pq.top();
-            
+            auto prime=pq.top();
             pq.pop();
+            int i=prime.second.first, j=prime.second.second;
             
-            int i=imp.second.first, j=imp.second.second;
+            ans.push_back({nums1[i], nums2[j]});
             
-            ans.push_back({nums1[i],nums2[j]});
-            
-            if(j+1<n2)
-            pq.push({nums1[i]+nums2[j+1],{i,j+1}});
+            if(i+1<n1){
+                pq.push({nums1[i+1]+nums2[j], {i+1, j}});
+            }
         }
         
         return ans;
