d31eb85ed764fecc8023d4ea9305d0f333dc64ab
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-08 12:16:54 +0530

    Time: 205 ms (55.92%), Space: 83.9 MB (56.10%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -8,9 +8,7 @@ public:
         int max_len=0;
         
         for(int i=0; i<n; i++){
-            //nums[i]==0?-1:1;
-            if(nums[i]==0)
-                nums[i]=-1;
+            nums[i]=nums[i]==0?-1:1;
             
             sum+=nums[i];
             
