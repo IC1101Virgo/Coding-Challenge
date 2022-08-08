ff3ed675f443bdae290aea92b2ef7ae97d78e3cd
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-08 16:16:22 +0530

    Time: 39 ms (95.73%), Space: 30.4 MB (8.15%) - LeetHub

---
+++ 697-degree-of-an-array/697-degree-of-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        int n=nums.size();
+        
+        unordered_map<int,vector<int>> mp;
+        
+        for(int i=0; i<n; i++){
+            mp[nums[i]].push_back(i);
+        }
+        
+        int degree=0;
+
+        for(auto x: mp){
+            int size=x.second.size();
+            degree=max(degree, size);
+        }
+        
+        int min_len=INT_MAX;
+        
+        for(auto x:mp){
+            if(x.second.size()==degree){
+            int mini= *min_element(x.second.begin(),x.second.end());
+                int maxi= *max_element(x.second.begin(),x.second.end());
+                
+                min_len= min(min_len, maxi-mini+1);
+                }
+        }
+        
+        return min_len;
+    }
+};
