0d2421d41d3cde49dab612379efd9e82b00dae97
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-09 12:52:55 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (56.76%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -4,18 +4,16 @@ public:
         int n=s.size();
         
         stack<pair<string, int>> stk;
+        
         int num=0;
         string ans="";
-        int i=0;
         
         for(int i=0; i<n; i++){
-            if(isdigit(s[i])){
-                num = num*10 + (s[i]-'0');
-            
-            }
-            
-            else if(isalpha(s[i]))
+            if(isalpha(s[i]))
                 ans+=s[i];
+            
+        else if(isdigit(s[i]))
+            num=num*10+(s[i]-'0');
             
             else if(s[i]=='['){
                 stk.push({ans,num});
@@ -23,18 +21,16 @@ public:
                 num=0;
             }
             
-            else if(s[i]==']')//As a ] is encountered we generate the given substring according to 'k' times given on stack top.We add the string at stack top and generated string so that it may be processed again if required
-            {
+            else if(s[i]==']'){
                 string tmp=ans;
                 
-                for(int i=1;i<stk.top().second;i++)                
-                   tmp+=ans;
+                for(int i=1; i<stk.top().second; i++){
+                    tmp+=ans;
+                }
                 
                 ans=stk.top().first+tmp;
                 stk.pop();
-                
             }
-            
         }
         
         return ans;
