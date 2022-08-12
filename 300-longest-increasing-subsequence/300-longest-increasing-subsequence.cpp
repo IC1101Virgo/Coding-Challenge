bcd581dea01758bb193b4e0a0c848a670901c699
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-12 10:13:50 +0530

    Time: 357 ms (58.49%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,14 +6,12 @@ public:
         vector<int> dp(n,1);
         int ans=INT_MIN;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j])
-                    dp[i]=max(dp[i],dp[j]+1);
-                
-                
-            }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j]<nums[i])
+                    dp[i]=max(dp[i],1+dp[j]);
             
+            }
             ans=max(ans,dp[i]);
         }
         
