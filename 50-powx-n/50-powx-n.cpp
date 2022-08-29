4ce94864decd9c3e0df0d1eff15dd6eb9ef91156
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-08-29 12:10:10 +0530

    Time: 8 ms (7.51%), Space: 5.9 MB (46.07%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,18 +3,23 @@ public:
     double myPow(double x, long n) {
         if(n==0)
             return 1;
+        
         if(x==0)
             return 0;
+        
         if(n==1)
             return x;
         
         if(n<0)
-            return myPow(1/x,-n);
-        double res= myPow(x*x,n/2);
+            return myPow(1/x, -n);
         
-        if(n%2!=0)
-            res*=x;
+        double num= myPow(x*x, n/2);
         
-        return res;
+        if(n%2 != 0)
+            num*=x;
+        
+        return num;
+        
+        
     }
 };
