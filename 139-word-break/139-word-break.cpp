f94ff8119391efe6c1bcd78df277e25d79c20465
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-09-05 11:28:20 +0530

    Time: 58 ms (12.53%), Space: 13.3 MB (41.43%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    bool wordBreak(string A, vector<string>& B) {
-           int n=A.size();
-    
-    set<string>di;
-    
-    for(auto x:B){
-        di.insert(x);
-    }
-    
-    vector<bool> dp(n+1,0);
-    dp[0]=1;
-    
-     for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                
-               if(dp[j]==1){
-                    string word=A.substr(j,i-j);
-                        
-                        if(di.find(word)!=di.end()){
-                            dp[i]=1;
-                   
-                        }
-               }
+    bool wordBreak(string a, vector<string>& b) {
+        set<string> di;
+        int n=a.size();
+        
+        for(auto x:b)
+            di.insert(x);
+        
+        vector<int> dp(n+1, 0);
+        dp[0]=1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j]==1){
+                    string word= a.substr(j, i-j);
+                    if(di.find(word)!=di.end())
+                        dp[i]=1;
+                }
             }
         }
-    
-    return dp[n];
+        
+        return dp[n];
     }
 };
