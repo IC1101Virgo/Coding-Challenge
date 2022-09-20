23c1e97611a8a585885390ef654cacafbc12bd1f
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-09-20 11:12:58 +0530

    Time: 4 ms (28.66%), Space: 7 MB (29.57%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& ma) {
-        vector<int> v;
-        
-        int n=ma.size(), m=ma[0].size();
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int n=mat.size(), m=mat[0].size();
         
         int top=0, down=n-1, left=0, right=m-1;
+        vector<int> v;
         
         while(top<=down && left<=right){
-            for(int i=left;i<=right;i++){
-                v.push_back(ma[top][i]);
+            for(int i=left; i<=right; i++){
+                v.push_back(mat[top][i]);
             }
             
             top++;
             
-            for(int i=top;i<=down;i++){
-                v.push_back(ma[i][right]);
+            for(int i=top; i<=down; i++){
+                v.push_back(mat[i][right]);
             }
             
             right--;
@@ -23,15 +22,18 @@ public:
             if(top>down || left>right)
                 break;
             
-            for(int i=right;i>=left;i--){
-                v.push_back(ma[down][i]);
+            for(int i=right; i>=left; i--){
+                v.push_back(mat[down][i]);
             }
+            
             down--;
             
-            for(int i=down;i>=top;i--)
-                v.push_back(ma[i][left]);
+            for(int i=down; i>=top; i--){
+                v.push_back(mat[i][left]);
+            }
             
             left++;
+            
         }
         
         return v;
