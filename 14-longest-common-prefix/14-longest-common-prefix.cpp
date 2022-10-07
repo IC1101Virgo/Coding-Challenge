57f476897b7bb21a0be6fd591ea323ef87942554
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-10-07 23:13:16 +0530

    Time: 10 ms (37.09%), Space: 9.2 MB (82.47%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,64 +1,23 @@
 class Solution {
 public:
-    struct Trie{
-        Trie *ischild[26];
-        bool isword;
-        int cnt;
-        Trie(){
-            for(int i=0;i<26;i++)
-                ischild[i]=NULL;
-            isword=false;
-            cnt=0;
-        }
-    };
-    
-    Trie *root;
-    
-    void insert(Trie *root, string str){
-        Trie *curr=root;
+    string longestCommonPrefix(vector<string>& strs) {
+        int i=1;
         
-        for(auto i:str){
-            if(curr->ischild[i-'a']==NULL){
-                Trie *temp=new Trie();
-                curr->ischild[i-'a']=temp;
+        string x=strs[0];
+        
+        while(i<strs.size()){
+            
+            for(int j=0; j<x.size(); j++){
+                if(x[j]!=strs[i][j])
+                {
+                    x=x.substr(0,j);
+                    break;
+                }
             }
             
-            curr=curr->ischild[i-'a'];
-            curr->cnt++;
+            i++;
         }
         
-        curr->isword=true;
-    }
-    
-    void search(Trie* root, string str, string &res, int len){
-        
-        Trie *curr=root;
-        
-        for(auto i:str){
-            if(curr->ischild[i-'a']==NULL)
-                break;
-            
-            if(curr->ischild[i-'a']->cnt== len)
-                res+=i;
-            
-            else break;
-            
-            curr =curr->ischild[i-'a'];
-            
-        }
-    }
-    
-    string longestCommonPrefix(vector<string>& strs) {
-        Trie *root=new Trie();
-        
-        for(int i=0;i<strs.size();i++){
-            insert(root,strs[i]);
-        }
-        
-        string res="";
-        
-        search(root,strs[0],res, strs.size());
-        
-        return res;
+        return x;
     }
 };
