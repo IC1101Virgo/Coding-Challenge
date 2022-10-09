7ad8b43f8cd402789998364bea1557805721b6e5
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2022-10-09 07:04:13 +0530

    Time: 12 ms (58.20%), Space: 13.1 MB (80.86%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        
         int n=grid.size(), m=grid[0].size();
         
-      
-        int org=0;
         queue<pair<int,int>> q;
+        
+        int orange=0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1)
-                    org++;
-                
                 if(grid[i][j]==2)
                     q.push({i,j});
+                
+                if(grid[i][j]==1)
+                    orange++;
             }
         }
         
         int time=0;
+        
         int dx[4]={1,-1,0,0};
         int dy[4]={0,0,1,-1};
         
-        while(!q.empty() && org!=0){
+        while(!q.empty() && orange){
             int size=q.size();
             
             while(size--){
-                auto temp=q.front();
+                auto [x,y]=q.front();
                 
                 q.pop();
                 
                 for(int k=0;k<4;k++){
-                    int x=temp.first+dx[k];
-                    int y=temp.second+dy[k];
+                    int a=x+dx[k];
+                    int b=y+dy[k];
                     
-                    if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1){
-                        grid[x][y]=2;
-                        org--;
-                        q.push({x,y});
-                        }
+                    if(a>=0 && a<n && b>=0 && b<m && grid[a][b]==1){
+                        orange--;
+                        grid[a][b]=2;
+                        q.push({a,b});
+                    }
                 }
+                
             }
             
             time++;
         }
         
-        if(org==0)
+        if(orange==0)
             return time;
         
         else return -1;
