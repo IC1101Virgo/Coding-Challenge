340b02ad7bd84795cdbb0c34c8b6354166c2bbb7
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2024-06-16 14:09:21 +0530

    Time: 52 ms (46.40%) | Memory: 48.2 MB (70.10%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,66 @@
+struct TrieNode{
+    TrieNode *ischild[26];
+    bool isword;
+
+    TrieNode(){
+        for(int i=0; i<26; i++){
+            ischild[i] = NULL;
+        }
+
+        isword = false;
+    }
+};
+class Trie {
+public:
+    TrieNode *root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode *curr = root;
+
+        for(auto x:word){
+            if(curr->ischild[x-'a']==NULL){
+                TrieNode* temp = new TrieNode();
+                curr->ischild[x-'a'] = temp;
+            }
+
+            curr = curr->ischild[x-'a'];
+        }
+         curr->isword = true;
+    }
+    
+    bool search(string word) {
+        TrieNode *curr = root;
+        for(auto x:word){
+            if(curr->ischild[x-'a']==NULL)
+            return false;
+
+            curr = curr->ischild[x-'a'];
+        }
+
+        return curr->isword;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode *curr = root;
+        for(auto x:prefix){
+            if(curr->ischild[x-'a']==NULL)
+            return false;
+
+            curr = curr->ischild[x-'a'];
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
