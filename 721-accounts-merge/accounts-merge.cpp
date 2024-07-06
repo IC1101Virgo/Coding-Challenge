8209f0b2e27b49057a5049b1df137195fc073369
Anjali Chaturvedi <59052808+IC1101Virgo@users.noreply.github.com>
2024-07-06 16:05:35 +0530

    Time: 61 ms (83.13%) | Memory: 38.1 MB (38.62%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,45 +1,59 @@
 class Solution {
 public:
     vector<int> parent;
-    int find_set(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find_set(parent[x]);
+
+    int find_s(int x){
+        if(parent[x] == x)
+        return x;
+
+        else return parent[x]=find_s(parent[x]);
     }
 
-    void make_union(int x, int y){
-        int a = find_set(x);
-        int b = find_set(y);
-        if(a != b){
-            parent[a] = b;
-        }
+    void make_union(int a, int b){
+        int aa = find_s(a), bb= find_s(b);
+
+        if(aa!=bb)
+        parent[aa] = bb;
+
     }
 
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
         parent.resize(n);
-        unordered_map<string, int> emailtoId;
-        for(int i = 0; i<n; i++){
-           parent[i] = i;
-           for(int j = 1; j<accounts[i].size(); j++){
-               if(emailtoId.find(accounts[i][j]) != emailtoId.end()){
-                   make_union(emailtoId[accounts[i][j]], i);
-               }
-               else{
-                   emailtoId[accounts[i][j]] = parent[i];
-               }
-           }
+
+        unordered_map<string, int> mp;
+
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+
+            for(int j=1; j<accounts[i].size(); j++){
+                if(mp.find(accounts[i][j])!=mp.end()){
+                    make_union(mp[accounts[i][j]], i);
+                }
+
+                else mp[accounts[i][j]] = parent[i];
+            }
         }
-        unordered_map<int, vector<string>> newaccounts;
-        for(auto it : emailtoId){
-            newaccounts[find_set(it.second)].push_back(it.first);
+
+        unordered_map<int, vector<string>> newacc;
+
+        for(auto it:mp){
+            newacc[find_s(it.second)].push_back(it.first);
         }
-        vector<vector<string>> ans;
-        for(auto &it : newaccounts){
-            auto &emails = it.second;
-                sort(emails.begin(), emails.end());
-                emails.insert(emails.begin(), accounts[it.first][0]);
-                ans.push_back(emails);
+
+        vector<vector<string>> res;
+
+        for(auto it: newacc){
+            auto mail = it.second;
+
+            sort(mail.begin(), mail.end());
+            mail.insert(mail.begin(), accounts[it.first][0]);
+
+            res.push_back(mail);
         }
-        return ans;
+
+        return res;
+
+
     }
 };
